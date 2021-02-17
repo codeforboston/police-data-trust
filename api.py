1fc7a48ccfbe5c6e908557909eeff6af96f52f65
Ryan Mahan <ryanmahan97@gmail.com>
2021-02-16 21:36:31 -0500

    add incident routes file

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from routes.incidents import incident_routes;
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
+
+app.register_blueprint(incident_routes)
+app.run()
