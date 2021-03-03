0f357fe5364c7b75eb278438ad456c949fbbc38d
Ryan Mahan <ryanmahan97@gmail.com>
2021-03-02 20:53:38 -0500

    GET route

--- incidents.py
+++ incidents.py
@@ -1,25 +1,29 @@
 """Define the SQL classes for Users."""
 
-from api import db
 from flask_sqlalchemy import SQLAlchemy
+from flask_serialize.flask_serialize import FlaskSerialize
 
-class Incidents(SQLAlchemy.Model):
+db = SQLAlchemy()
+fs_mixin = FlaskSerialize(db)
+
+class Incidents(db.Model, fs_mixin):
 		"""The SQL dataclass for an Incident."""
 
-		__tablename__ = "incidents"
+		__tablename__ = "incident"
 
-		Incident_ID = db.Column(db.Unicode(512), primary_key=True)
-		Occurence_Date = db.Column(db.DateTime)  # noqa: N815
-		State_Abbv = db.Column(db.Unicode(512))
-		City = db.Column(db.Unicode(512))
-		Address_1 = db.Column(db.Unicode(512))
-		Address_2 = db.Column(db.Unicode(512))
-		Zip_Code = db.Column(db.Unicode(512))
-		Stop_Type = db.Column(db.Unicode(512))
-		Call_Type = db.Column(db.Unicode(512))
-		Has_Multimedia = db.Column(db.Boolean)
-		From_Report = db.Column(db.Boolean)
-		Race = db.Column(db.Boolean)
-		Neighborhood = db.Column(db.Unicode(512))
+		incident_id = db.Column(db.Unicode(512), primary_key=True)
+		occurence_date = db.Column(db.DateTime)  # noqa: N815
+		state_abbv = db.Column(db.Unicode(512))
+		city = db.Column(db.Unicode(512))
+		address_1 = db.Column(db.Unicode(512))
+		address_2 = db.Column(db.Unicode(512))
+		zip_code = db.Column(db.Unicode(512))
+		stop_type = db.Column(db.Unicode(512))
+		call_type = db.Column(db.Unicode(512))
+		has_multimedia = db.Column(db.Boolean)
+		from_report = db.Column(db.Boolean)
+		race = db.Column(db.Boolean)
+		neighborhood = db.Column(db.Unicode(512))
 
-		role = db.relationship("RoleTable", backref=db.backref("incidents", lazy=True))
+		# role = db.relationship("RoleTable", backref=db.backref("incidents", lazy=True))
+		
--- routes/incidents.py
+++ routes/incidents.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from incidents import Incidents
 incident_routes = Blueprint("incident_routes", __name__, url_prefix="/incidents")
 
 @incident_routes.route("/")
-def get_all_incidents():
-  return "All incidents!";
-
 @incident_routes.route("/<id>")
 def get_single_incident(id):
-  return "Incident: " + id;
+  return Incidents.get_delete_put_post(id)
+
+@incident_routes.route("/", methods=["POST"])
+def create_incident():
+  Incidents.get_delete_put_post()
+  return "201"
+  # return jsonify(inc)
