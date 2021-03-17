4b6aef76257cb03a5ed91a56101b30ada6f28c6f
Ryan Mahan <ryanmahan97@gmail.com>
2021-03-16 21:33:28 -0400

    update sql script

--- sql/incidents.sql
+++ sql/incidents.sql
@@ -1,9 +1,11 @@
--- police_data.incidents definition
+-- public.incidents definition
 
 -- Drop table
 
-DROP TABLE police_data.incidents;
-DROP TYPE cause_of_death_enum;
+-- DROP TABLE public.incidents;
+
+DROP TYPE IF EXISTS cause_of_death_enum cascade;
+
 
 CREATE TYPE cause_of_death_enum AS ENUM (
 	'Unknown',
@@ -12,7 +14,7 @@ CREATE TYPE cause_of_death_enum AS ENUM (
 	'Choke',
 	'Other');
 
-DROP TYPE gender_enum;
+DROP TYPE IF EXISTS gender_enum cascade;
 
 CREATE TYPE gender_enum AS ENUM (
 	'Unknown',
@@ -20,7 +22,7 @@ CREATE TYPE gender_enum AS ENUM (
 	'Female',
 	'Transgender');
 
-DROP TYPE initial_encounter_enum;
+DROP TYPE IF EXISTS initial_encounter_enum cascade;
 
 CREATE TYPE initial_encounter_enum AS ENUM (
 	'Unknown',
@@ -29,7 +31,7 @@ CREATE TYPE initial_encounter_enum AS ENUM (
 	'PotentialCrimeSuspect',
 	'Other');
 
-DROP TYPE race_enum;
+DROP TYPE IF EXISTS race_enum cascade;
 
 CREATE TYPE race_enum AS ENUM (
 	'Unknown',
@@ -39,7 +41,7 @@ CREATE TYPE race_enum AS ENUM (
 	'Asian',
 	'Native_Hawaiian_Pacific_Islander');
 
-DROP TYPE status_enum;
+DROP TYPE IF EXISTS status_enum cascade;
 
 CREATE TYPE status_enum AS ENUM (
 	'Unknown',
@@ -48,7 +50,7 @@ CREATE TYPE status_enum AS ENUM (
 	'Disabled',
 	'Deceased');
 
-DROP TYPE victim_action_enum;
+DROP TYPE IF EXISTS victim_action_enum cascade;
 
 CREATE TYPE victim_action_enum AS ENUM (
 	'Unknown',
@@ -59,7 +61,7 @@ CREATE TYPE victim_action_enum AS ENUM (
 	'Attacking',
 	'Other');
 
-DROP TYPE victim_weapon_enum;
+DROP TYPE IF EXISTS victim_weapon_enum cascade;
 
 CREATE TYPE victim_weapon_enum AS ENUM (
 	'Unknown',
@@ -69,55 +71,57 @@ CREATE TYPE victim_weapon_enum AS ENUM (
 	'NoWeapon',
 	'Other');
 
-
-CREATE TABLE police_data.incidents (
+DROP TABLE IF EXISTS public.incidents;
+CREATE TABLE public.incidents (
 	"Incident_ID" serial NOT NULL,
 	"Occurrence_Date" timestamp NULL,
-	"State_Abbv" text NULL,
-	"City" text NULL,
-	"Address_1" text NULL,
-	"Address_2" text NULL,
+	"State_Abbv" varchar(512) NULL,
+	"City" varchar(512) NULL,
+	"Address_1" varchar(512) NULL,
+	"Address_2" varchar(512) NULL,
 	"Zip_Code" text NULL,
 	"Latitude" float8 NULL,
 	"Longitude" float8 NULL,
 	"Reported_Date" timestamp NULL,
 	"Initial_Reason_For_Encounter" initial_encounter_enum NULL,
-	"Charges_Involved" text NULL,
+	"Charges_Involved" varchar(512) NULL,
 	"Victim_Weapon" victim_weapon_enum NULL,
 	"Victim_Action" victim_action_enum NULL,
 	"Has_Multimedia" bool NULL,
-	"Media_URL" text NULL,
+	"Media_URL" varchar(512) NULL,
 	"From_Report" bool NULL,
-	"Description" text NULL,
-	"Associated_Incidents" text NULL,
+	"Description" varchar(512) NULL,
+	"Associated_Incidents" varchar(512) NULL,
 	"Death_Date" timestamp NULL,
-	"Death_State_Abbv" text NULL,
-	"Death_City" text NULL,
-	"Death_Address_1" text NULL,
-	"Death_Address_2" text NULL,
+	"Death_State_Abbv" varchar(512) NULL,
+	"Death_City" varchar(512) NULL,
+	"Death_Address_1" varchar(512) NULL,
+	"Death_Address_2" varchar(512) NULL,
 	"Death_Zip_Code" text NULL,
 	"Cause_Of_Death" cause_of_death_enum NULL,
-	"Cause_Of_Death_Description" text NULL,
-	"First_Name" text NULL,
-	"Last_Name" text NULL,
+	"Cause_Of_Death_Description" varchar(512) NULL,
+	"First_Name" varchar(512) NULL,
+	"Last_Name" varchar(512) NULL,
 	"Age_At_Incident" int4 NULL,
 	"Gender" gender_enum NULL,
 	"Race" race_enum NULL,
 	"Status" status_enum NULL,
 	"Agency_ID" int4 NULL,
-	"Agency_Name" text NULL,
-	"Agency_State_Abbv" text NULL,
-	"Agency_City" text NULL,
-	"Agency_Address_1" text NULL,
-	"Agency_Address_2" text NULL,
+	"Agency_Name" varchar(512) NULL,
+	"Agency_State_Abbv" varchar(512) NULL,
+	"Agency_City" varchar(512) NULL,
+	"Agency_Address_1" varchar(512) NULL,
+	"Agency_Address_2" varchar(512) NULL,
 	"Agency_Zip_Code" text NULL,
 	"Agency_Latitude" float8 NULL,
 	"Agency_Longitude" float8 NULL,
 	CONSTRAINT incidents_pkey PRIMARY KEY ("Incident_ID")
 );
 
-INSERT INTO police_data.incidents ("Occurrence_Date","State_Abbv","City","Address_1","Address_2","Zip_Code","Latitude","Longitude","Reported_Date","Initial_Reason_For_Encounter","Charges_Involved","Victim_Weapon","Victim_Action","Has_Multimedia","Media_URL","From_Report","Description","Associated_Incidents","Death_Date","Death_State_Abbv","Death_City","Death_Address_1","Death_Address_2","Death_Zip_Code","Cause_Of_Death","Cause_Of_Death_Description","First_Name","Last_Name","Age_At_Incident","Gender","Race","Status","Agency_ID","Agency_Name","Agency_State_Abbv","Agency_City","Agency_Address_1","Agency_Address_2","Agency_Zip_Code","Agency_Latitude","Agency_Longitude") VALUES
-	 ('2021-03-01 00:00:00.000','MA','Boston','1 Commonwealth Ave','','02215',NULL,NULL,NULL,'Trespassing'::initial_encounter_enum::initial_encounter_enum,'None','Firearm'::victim_weapon_enum::victim_weapon_enum,'Fleeing'::victim_action_enum::victim_action_enum,false,NULL,true,'This is an example incident description',NULL,'2020-04-01 00:00:00.000','MA','Boston','120 Ward St',NULL,'02120','BluntForce'::cause_of_death_enum::cause_of_death_enum,NULL,'Jane','Smith',25,'Female'::gender_enum::gender_enum,'Black_African_American'::race_enum::race_enum,'Deceased'::status_enum::status_enum,NULL,'Boston Police Department','MA','Boston','1 Schroder Plaza',NULL,'02120',42.3339686,-71.0907954);
+INSERT INTO public.incidents
+("Incident_ID", "Occurrence_Date", "State_Abbv", "City", "Address_1", "Address_2", "Zip_Code", "Latitude", "Longitude", "Reported_Date", "Initial_Reason_For_Encounter", "Charges_Involved", "Victim_Weapon", "Victim_Action", "Has_Multimedia", "Media_URL", "From_Report", "Description", "Associated_Incidents", "Death_Date", "Death_State_Abbv", "Death_City", "Death_Address_1", "Death_Address_2", "Death_Zip_Code", "Cause_Of_Death", "Cause_Of_Death_Description", "First_Name", "Last_Name", "Age_At_Incident", "Gender", "Race", "Status", "Agency_ID", "Agency_Name", "Agency_State_Abbv", "Agency_City", "Agency_Address_1", "Agency_Address_2", "Agency_Zip_Code", "Agency_Latitude", "Agency_Longitude")
+VALUES(1, '2021-03-01 00:00:00.000', 'MA', 'Boston', '1 Commonwealth Ave', '', '02215', NULL, NULL, NULL, 'Trespassing'::initial_encounter_enum::initial_encounter_enum, 'None', 'Firearm'::victim_weapon_enum::victim_weapon_enum, 'Fleeing'::victim_action_enum::victim_action_enum, false, NULL, true, 'This is an example incident description', NULL, '2020-04-01 00:00:00.000', 'MA', 'Boston', '120 Ward St', NULL, '02120', 'BluntForce'::cause_of_death_enum::cause_of_death_enum, NULL, 'Jane', 'Smith', 25, 'Female'::gender_enum::gender_enum, 'Black_African_American'::race_enum::race_enum, 'Deceased'::status_enum::status_enum, NULL, 'Boston Police Department', 'MA', 'Boston', '1 Schroder Plaza', NULL, '02120', 42.3339686, -71.0907954);
 
-INSERT INTO police_data.incidents ("Occurrence_Date","State_Abbv","City","Address_1","Address_2","Zip_Code","Latitude","Longitude","Reported_Date","Initial_Reason_For_Encounter","Charges_Involved","Victim_Weapon","Victim_Action","Has_Multimedia","Media_URL","From_Report","Description","Associated_Incidents","Death_Date","Death_State_Abbv","Death_City","Death_Address_1","Death_Address_2","Death_Zip_Code","Cause_Of_Death","Cause_Of_Death_Description","First_Name","Last_Name","Age_At_Incident","Gender","Race","Status","Agency_ID","Agency_Name","Agency_State_Abbv","Agency_City","Agency_Address_1","Agency_Address_2","Agency_Zip_Code","Agency_Latitude","Agency_Longitude") VALUES
-	 ('2021-03-26 00:00:00.000','MA','Allston','1000 Commonwealth Ave','Apt 101','02215',42.3539,71.1337,'2020-02-20 00:00:00.000','TrafficViolation'::initial_encounter_enum::initial_encounter_enum,'Arrest','Blunt'::victim_weapon_enum::victim_weapon_enum,'Speaking'::victim_action_enum::victim_action_enum,false,'',false,'lorem ipsum sit dolor amet',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'John','Doe',30,'Male'::gender_enum::gender_enum,'Unknown'::race_enum::race_enum,'Injured'::status_enum::status_enum,10,'Boston Police Department','MA','Boston','1 Schroeder Plaza','','02120',42.3339686,-71.0907954);
+INSERT INTO public.incidents
+("Incident_ID", "Occurrence_Date", "State_Abbv", "City", "Address_1", "Address_2", "Zip_Code", "Latitude", "Longitude", "Reported_Date", "Initial_Reason_For_Encounter", "Charges_Involved", "Victim_Weapon", "Victim_Action", "Has_Multimedia", "Media_URL", "From_Report", "Description", "Associated_Incidents", "Death_Date", "Death_State_Abbv", "Death_City", "Death_Address_1", "Death_Address_2", "Death_Zip_Code", "Cause_Of_Death", "Cause_Of_Death_Description", "First_Name", "Last_Name", "Age_At_Incident", "Gender", "Race", "Status", "Agency_ID", "Agency_Name", "Agency_State_Abbv", "Agency_City", "Agency_Address_1", "Agency_Address_2", "Agency_Zip_Code", "Agency_Latitude", "Agency_Longitude")
+VALUES(2, '2021-03-26 00:00:00.000', 'MA', 'Allston', '1000 Commonwealth Ave', 'Apt 101', '02215', 42.3539, 71.1337, '2020-02-20 00:00:00.000', 'TrafficViolation'::initial_encounter_enum::initial_encounter_enum, 'Arrest', 'Blunt'::victim_weapon_enum::victim_weapon_enum, 'Speaking'::victim_action_enum::victim_action_enum, false, '', false, 'lorem ipsum sit dolor amet', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'John', 'Doe', 30, 'Male'::gender_enum::gender_enum, 'Unknown'::race_enum::race_enum, 'Injured'::status_enum::status_enum, 10, 'Boston Police Department', 'MA', 'Boston', '1 Schroeder Plaza', '', '02120', 42.3339686, -71.0907954);
