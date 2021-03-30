26c3f5dde3ff8952f9e84882a7400ff718cc5dc6
stianberghansen <stian@berghansen.me>
2021-03-30 18:22:49 -0400

    Adds worker file with celery beat schedule

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,14 +1,12 @@
 from celery import Celery
-# from celery.schedules import crontab
-from celery.utils.log import get_task_logger
 from incidents import Incidents
 from config import Config
-import requests
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from worker import app
+import requests
 
-logger = get_task_logger(__name__)
 
 config = Config()
 
@@ -18,30 +16,36 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
 
-app = Celery('tasks', backend='redis://redis:6379/0',
-             broker='redis://redis:6379/0')
-
-
 def get_or_create(session, model, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).first()
-    if instance:
-        # if instance exsits
-        # TODO: check if instance needs updating
-        return instance
-    else:
-        instance = model(**kwargs)
-        session.add(instance)
-        session.commit()
-        return instance
+    # Update to allow for checking original incident ID
+    try:
+        instance = session.query(model).filter_by(**kwargs).first()
+        if instance:
+            # if instance exsits
+            # TODO: check if instance needs updating
+            return instance
+        else:
+            instance = model(**kwargs)
+            session.add(instance)
+            session.commit()
+            return instance
+    except Exception as err:
+        raise
 
 
-@app.task
-def import_data(url):
+@app.task(bind=True, name='refresh')
+def refresh(self, urls):
     session = Session()
+    for url in urls:
+        fetch_data(url)
+
+
+@app.task(bind=True, name='fetch_data')
+def fetch_data(self, url):
     res = requests.get(url)
-
-    if res.content:
-        for data in res.data:
-            get_or_create(session, Incidents, data)
-
-    return "Data added"
+    try:
+        if res.data:
+            for data in res.data:
+                get_or_create(session, Incidents, data)
+    except Exception as err:
+        raise
