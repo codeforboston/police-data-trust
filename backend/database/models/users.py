1fdf79ea299e0636ede894023394104bf1f2b084
Stampeder525 <lorenheubertaubry@gmail.com>
2021-05-06 17:00:14 -0400

    minor sqlalchemy fixes

--- backend/database/models/users.py
+++ backend/database/models/users.py
@@ -24,13 +24,13 @@ class CI_String(TypeDecorator):
   """ Case-insensitive String subclass definition"""
   impl = String
   def __init__(self, length, **kwargs):
-      if kwargs.get('collate'):
-          if kwargs['collate'].upper() not in ['BINARY','NOCASE','RTRIM']:
-              raise TypeError("%s is not a valid SQLite collation" % kwargs['collate'])
-          self.collation = kwargs.pop('collate').upper()
+      if kwargs.get("collate"):
+          if kwargs["collate"].upper() not in ["BINARY","NOCASE","RTRIM"]:
+              raise TypeError("%s is not a valid SQLite collation" % kwargs["collate"])
+          self.collation = kwargs.pop("collate").upper()
       super(CI_String, self).__init__(length=length, **kwargs)
 
-@compiles(CI_String, 'sqlite')
+@compiles(CI_String, "sqlite")
 def compile_ci_string(element, compiler, **kwargs):
   base_visit = compiler.visit_string(element, **kwargs) 
   if element.collation:
@@ -45,23 +45,23 @@ class Users(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+    active = db.Column("is_active", db.Boolean(), nullable=False, server_default="1")
 
-    # User authentication information. The collation='NOCASE' is required
-    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
-    email = db.Column(CI_String(255, collation='NOCASE'), nullable=False, unique=True)
+    # User authentication information. The collation="NOCASE" is required
+    # to search case insensitively when USER_IFIND_MODE is "nocase_collation".
+    email = db.Column(CI_String(255, collate="NOCASE"), nullable=False, unique=True)
     email_confirmed_at = db.Column(db.DateTime())
-    password = db.Column(db.String(255), nullable=False, server_default='')
+    password = db.Column(db.String(255), nullable=False, server_default="")
 
     # User information
-    first_name = db.Column(CI_String(100, collation='NOCASE'), nullable=False, server_default='')
-    last_name = db.Column(CI_String(100, collation='NOCASE'), nullable=False, server_default='')
+    first_name = db.Column(CI_String(100, collate="NOCASE"), nullable=False, server_default="")
+    last_name = db.Column(CI_String(100, collate="NOCASE"), nullable=False, server_default="")
 
     # Define the relationship to Role via UserRoles
-    roles = db.relationship('Role', secondary='user_roles')
+    roles = db.relationship("Role", secondary="user_roles")
 
     def verify_password(self, pw):
-        return bcrypt.checkpw(pw.encode('utf8'), self.password.encode('utf8'))
+        return bcrypt.checkpw(pw.encode("utf8"), self.password.encode("utf8"))
 
 
 db_adapter = SQLAlchemyAdapter(db, Users)
@@ -69,13 +69,13 @@ user_manager = UserManager(db_adapter)
 
  # Define the Role data-model
 class Role(db.Model):
-    __tablename__ = 'roles'
+    __tablename__ = "roles"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
 
 # Define the UserRoles association table
 class UserRoles(db.Model):
-    __tablename__ = 'user_roles'
+    __tablename__ = "user_roles"
     id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id", ondelete="CASCADE"))
+    role_id = db.Column(db.Integer(), db.ForeignKey("roles.id", ondelete="CASCADE"))
--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -5,6 +5,8 @@ import pytest
     ("page", "expected_status_code"),
     [
         ("/incidents", 200),
+        ("/login", 200),
+        ("/register", 200)
     ],
 )
 def test_routes(client, page, expected_status_code):
