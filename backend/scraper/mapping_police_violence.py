80b5d05572f52fde083762fa9e09a17959bdab7a
arutfield <36383013+arutfield@users.noreply.github.com>
2021-05-23 18:23:10 -0400

    merges two sites into excel document

--- backend/scraper/mapping_police_violence.py
+++ backend/scraper/mapping_police_violence.py
@@ -6,6 +6,7 @@ import zipfile
 import io
 import os
 
+next_index = 0
 
 # data manipulation functions
 
@@ -13,9 +14,12 @@ import os
 def map_varnames(df, data_source, xwalk):
     # clean column names
     df.columns = (
-        df.columns.str.replace(" ", "_").str.replace
-        ("[^\\d\\w]", "").str.lower()
+       df.columns.str.replace(" ", "_").str.replace("[^\\d\\w]", "").str.lower()
     )
+    for i in range(xwalk.shape[0]):
+        if xwalk[data_source][i] not in df.columns:
+            df[xwalk[data_source][i]] = ""
+
     # assert all(xwalk["mpv"].isin(df.columns))
     # assert all(xwalk["nyclu"].isin(df.columns))
 
@@ -25,16 +29,23 @@ def map_varnames(df, data_source, xwalk):
     }
 
     df = df[xwalk[data_source].tolist()].rename(columns=name_map)
-
     return df
 
 
 def gen_ids(df, data_source):
+    global next_index
     # TODO: Decide how to make these ids, ex hashing columns
     df["data_source_id"] = data_source
-    df["incident_id"] = np.nan
+    df["incident_id"] = ""
     df["victim_id"] = np.nan
     df["death_id"] = np.nan
+    df.set_index("incident_id", inplace=True)
+    as_list = df.index.tolist()
+    for v in range(0, len(as_list)):
+        as_list[v] = next_index
+        next_index = next_index + 1
+    df.index = as_list
+
     return df
 
 
@@ -65,7 +76,7 @@ def extract_all_subfolders(head_directory, data_source, xwalk, dat):
     elif head_directory.endswith("csv.gz"):
         data = pd.read_csv(head_directory, nrows=1048576, compression='gzip',
                            error_bad_lines=False)
-        print(data)
+        # print(data)
         return data
 
 
@@ -74,23 +85,30 @@ def make_tables_data_source(data_source, xwalk, configs):
         dat_raw = pd.read_excel(configs["sources"][data_source]["url"])
         dat = map_varnames(dat_raw, data_source, xwalk)
         dat = gen_ids(dat, data_source)
+
+        print(dat.index)
     elif configs["sources"][data_source]["url"].endswith(".zip"):
         r = requests.get(configs["sources"][data_source]["url"])
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall(".")
         dat_raw = extract_all_subfolders("fully-unified-data",
                                          data_source, xwalk, [])
-        dat_raw['victims_name'] = ""
-        dat_raw['cause_of_death'] = ""
-        dat_raw['county'] = ""
         dat_raw = dat_raw.groupby(['cr_id']).first().reset_index()
         dat = map_varnames(dat_raw, data_source, xwalk)
         dat = gen_ids(dat, data_source)
-        # dat.to_csv(r'cpdp_data.csv', index=False, header=True)
+    elif configs["sources"][data_source]["url"].endswith(".csv"):
+        dat_raw = pd.read_csv(configs["sources"][data_source]["url"])
+        for i in range(xwalk.shape[0]):
+            if xwalk[data_source][i] not in dat_raw.columns:
+                dat_raw[xwalk[data_source][i]] = ""
+        dat = map_varnames(dat_raw, data_source, xwalk)
+        dat = gen_ids(dat, data_source)
+
+    dat = dat.loc[~dat.index.duplicated(keep='first')]
+    dat.to_csv(data_source + '.csv', index=True, header=True)
     table_names = list(configs["tables"].keys())
     table_list = [make_single_table(x, dat, configs) for x in table_names]
     table_dict = {table_names[i]: table_list[i] for i in range(len(table_list))}
-
     return table_dict
 
 
@@ -112,9 +130,29 @@ def make_all_tables():
         make_tables_data_source(x, xwalk, configs) for x in data_sources
     ]
     table_names = list(configs["tables"].keys())
-    table_list = [pd.concat([d[t] for d in data_list]) for t in table_names]
+    table_list = []
+    for t in table_names:
+        first_iteration = True
+        sub_table_list = []
+        for d in data_list:
+            if first_iteration:
+                full_df = d[t]
+                first_iteration = False
+            else:
+                full_df = pd.concat([full_df, d[t]])
+            sub_table_list.append(d[t])
+        sub_table = full_df
+        table_list.append(sub_table)
     table_dict = {table_names[i]: table_list[i] for i in range(len(table_list))}
-    # print(table_dict)
+    writer = pd.ExcelWriter('full_database.xlsx', engine='xlsxwriter')
+
+    # Write each dataframe to a different worksheet.
+    for key in table_dict:
+        sub_data = table_dict[key]
+        sub_data.to_excel(writer, sheet_name=key)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
     return table_dict
 
 
