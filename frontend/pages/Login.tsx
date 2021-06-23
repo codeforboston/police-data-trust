f7abc277c841c95f40b9073a235b0e9e90613ddc
Abhinav <akumar754@gatech.edu>
2021-06-23 18:00:38 -0400

    Revert "--"Forgot your username or password?" button added"
    This reverts commit 571c1a1a

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -189,14 +189,6 @@ const Login = () => {
                         Login
                     </Button>
                 </CardActions>
-                <CardActions>
-                    <Button
-                        variant="outlined"
-                        size="small"
-                        color="secondary">
-                        Forgot your username or password?
-                    </Button>
-                </CardActions>
             </Card>
         </form>
     );
