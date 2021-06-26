edb5c5f95942beb52dfb5b4edbd753ae596b1fa9
Joseph Richardson <jrichardson@jmstudios.net>
2021-06-25 17:14:43 -0700

    import of svgs for logos will now compile and be used as a component

---
+++ frontend/compositions/logo/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import LogoSVG from "./assets/NPDCLogo.svg"
+
+export default function Logo() {
+  return (
+    <div className="Logo">
+      <LogoSVG />
+    </div>
+  )
+}
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import "./login/style.css"
+import "./style.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
