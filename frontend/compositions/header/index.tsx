9bc3eba72111ac0226daec4c68e2f6ef92b33f7f
Nancy Yang <nyyang.194@gmail.com>
2021-07-05 23:26:24 -0700

    Updated CSS to use grid, modified nav bar, added onClick functionality to nav links.

--- frontend/compositions/header/index.tsx
+++ frontend/compositions/header/index.tsx
@@ -1,22 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import NPDCLogo from './assets/NPDCLogo.svg'
 import styles from './header.module.css'
 
+
 export default function Header() {
+  let [selected, setSelected] = useState('Search');
+
+  const handleNavChange = (e: any) => {
+    let value = e.target.innerText;
+    setSelected(value);
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <NPDCLogo />
-      </div>
-      <div className={styles.titleContainer}>
-          <h2>National Police Data Coalition</h2>
+    <header className={styles.container}>
+      <div className={styles.banner}>
+        <div className={styles.logo}>
+          <NPDCLogo />
+        </div>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>National Police Data Coalition</h2>
           <p className={styles.subtitle}>The national index of police incidents</p>
+        </div>
+        <nav>
+          <ul className={styles.nav}>
+            <li className={selected === 'Search' ? styles.tab : ''} onClick={handleNavChange}>Search</li>
+            <li className={selected === 'Profile' ? styles.tab : ''} onClick={handleNavChange}>Profile</li>
+            <li className={selected === 'About' ? styles.tab : ''} onClick={handleNavChange}>About</li>
+          </ul>
+        </nav>
+        <div className={styles.item4}>
+          Grid Item 4
+        </div>
       </div>
-      <div className={styles.links}>
-        <button className={styles.search}>Search</button>
-        <button>My Profile</button>
-        <button>About Us</button>
-      </div>
-    </div>
+    </header>
   )
 }
