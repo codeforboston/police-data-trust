5332ecf3f01c85f82af378d4b2e95abb3b1ef5d3
Nancy Yang <nyyang.194@gmail.com>
2021-07-08 20:57:36 -0700

    Updated grid layout to two columns using fr. Cleaned up CSS naming, removed unnecessary classNames.

--- frontend/compositions/header/index.tsx
+++ frontend/compositions/header/index.tsx
@@ -12,27 +12,27 @@ export default function Header() {
     setSelected(value);
   }
 
-  const { wrapper, banner, logo, titleContainer, title, subtitle, nav, tab, donate } = styles;
+  const { wrapper, banner, logoTitle, titleContainer, nav, tab, donate, navDonate} = styles;
 
   return (
     <header className={wrapper}>
       <div className={banner}>
-        <div className={logo}>
+        <div className={logoTitle}>
           <NPDCLogo />
+          <div className={titleContainer}>
+            <h2>National Police Data Coalition</h2>
+            <p>The national index of police incidents</p>
+          </div>
         </div>
-        <div className={titleContainer}>
-          <h2 className={title}>National Police Data Coalition</h2>
-          <p className={subtitle}>The national index of police incidents</p>
-        </div>
-        <nav>
-          <ul className={nav}>
-            <li className={selected === 'Search' ? tab : ''} onClick={handleNavChange}>Search</li>
-            <li className={selected === 'Profile' ? tab : ''} onClick={handleNavChange}>Profile</li>
-            <li className={selected === 'About' ? tab : ''} onClick={handleNavChange}>About</li>
-          </ul>
-        </nav>
-        <div className={donate}>
-          <button type="button">DONATE</button>
+        <div className={navDonate}>
+          <nav>
+            <ul className={nav}>
+              <li className={selected === 'Search' ? tab : ''} onClick={handleNavChange}>Search</li>
+              <li className={selected === 'Profile' ? tab : ''} onClick={handleNavChange}>Profile</li>
+              <li className={selected === 'About' ? tab : ''} onClick={handleNavChange}>About</li>
+            </ul>
+          </nav>
+          <button className={donate}type="button">DONATE</button>
         </div>
       </div>
     </header>
