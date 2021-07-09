c9cfd530a4a28049dd6425c0006674edbdce666d
Nancy Yang <nyyang.194@gmail.com>
2021-07-08 17:06:24 -0700

    Deconstructed classNames from styles

--- frontend/compositions/header/index.tsx
+++ frontend/compositions/header/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import NPDCLogo from './assets/NPDCLogo.svg'
-import styles from './header.module.css'
+import styles  from './header.module.css'
+
 
 
 export default function Header() {
@@ -11,24 +12,26 @@ export default function Header() {
     setSelected(value);
   }
 
+  const { wrapper, banner, logo, titleContainer, title, subtitle, nav, tab, donate } = styles;
+
   return (
-    <header className={styles.wrapper}>
-      <div className={styles.banner}>
-        <div className={styles.logo}>
+    <header className={wrapper}>
+      <div className={banner}>
+        <div className={logo}>
           <NPDCLogo />
         </div>
-        <div className={styles.titleContainer}>
-          <h2 className={styles.title}>National Police Data Coalition</h2>
-          <p className={styles.subtitle}>The national index of police incidents</p>
+        <div className={titleContainer}>
+          <h2 className={title}>National Police Data Coalition</h2>
+          <p className={subtitle}>The national index of police incidents</p>
         </div>
         <nav>
-          <ul className={styles.nav}>
-            <li className={selected === 'Search' ? styles.tab : ''} onClick={handleNavChange}>Search</li>
-            <li className={selected === 'Profile' ? styles.tab : ''} onClick={handleNavChange}>Profile</li>
-            <li className={selected === 'About' ? styles.tab : ''} onClick={handleNavChange}>About</li>
+          <ul className={nav}>
+            <li className={selected === 'Search' ? tab : ''} onClick={handleNavChange}>Search</li>
+            <li className={selected === 'Profile' ? tab : ''} onClick={handleNavChange}>Profile</li>
+            <li className={selected === 'About' ? tab : ''} onClick={handleNavChange}>About</li>
           </ul>
         </nav>
-        <div className={styles.donate}>
+        <div className={donate}>
           <button type="button">DONATE</button>
         </div>
       </div>
