4d83fe4a8e5289971094edd3f151e80d5fb5dd7c
Nancy Yang <nyyang.194@gmail.com>
2021-07-12 17:48:49 -0700

    Factored out desktop navigation bar to separate component, added anchor tags to list items, created setClassName helper function to DRY up code.

---
+++ frontend/compositions/header/desktopNav.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import styles  from './header.module.css'
+
+export default function DesktopNav() {
+  let [selected, setSelected] = useState('Search');
+
+  const handleNavChange = (e: React.MouseEvent<Element>) => {
+    const value = (e.target as HTMLElement).innerText;
+    setSelected(value);
+  }
+
+  const setClassName = (linkName: string) => {
+    if (linkName === selected) {
+      return styles.tab;
+    }
+    return '';
+  }
+
+  return (
+    <nav>
+      <ul className={styles.nav} onClick={handleNavChange}>
+        <li className={setClassName('Search')} ><a href='#'>Search</a></li>
+        <li className={setClassName('Profile')} ><a href='#'>Profile</a></li>
+        <li className={setClassName('About')}><a href='#'>About</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+
+
