d7da6eaed61a9960e3f74c8a6035a6bd788b83f1
michaelc <dylanesque27@gmail.com>
2021-07-23 14:58:49 -0400

    data-table successfully rendered

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,3 +1,12 @@
+import * as React from 'react';
+
+import { DataTable } from "../../shared-components/data-table/data-table"
+
 export default function Dashboard() {
-  return <div className="Dashboard">I AM DASHBORD!!!!!!</div>
+  return (
+    <>
+      <div className="Dashboard">I AM DASHBORD!!!!!!</div>
+      <DataTable />
+    </>
+  )
 }
--- frontend/shared-components/data-table/data-table.tsx
+++ frontend/shared-components/data-table/data-table.tsx
@@ -1,115 +1,143 @@
-import * as React from 'react';
+import * as React from "react"
 
 import { useTable } from "react-table"
 
 type IncidentData = {
-    dates: string;
-    incidentType: string;
-    officersInvolved: string[];
-    subject: string;
-    source: string;
+  dates: string
+  incidentType: string
+  officersInvolved: string[]
+  subject: string
+  source: string
 }
 
-// Defines table rows
-// useMemo is being used here to 
-const data = React.useMemo(
+export function DataTable() {
+  // Defines table rows
+  const data = React.useMemo(
     () => [
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
-        {
-            dates: "2003/01/01",
-            incidentType: "Use of force",
-            officersInvolved: ["John Smith"],
-            subject: "unknown",
-            source: "News Article",
-        },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
+      {
+        dates: "2003/01/01",
+        incidentType: "Use of force",
+        officersInvolved: ["John Smith"],
+        subject: "unknown",
+        source: "News Article",
+      },
     ],
     []
-);
+  )
 
-const columns = React.useMemo(
+  const columns = React.useMemo(
     () => [
-        {
-            Header: "Date(s)",
-            accessor: "dates" as const,
-        },
-        {
-            Header: "Incident Type",
-            accessor: "incidentType" as const,
-        },
-        {
-            Header: "Officer(s) Involved",
-            accessor: "officersInvolved" as const,
-        },
-        {
-            Header: "Subject",
-            accessor: "subject" as const,
-        },
-        {
-            Header: "Source",
-            accessor: "source" as const,
-        },
+      {
+        Header: "Date(s)",
+        accessor: "dates" as const,
+      },
+      {
+        Header: "Incident Type",
+        accessor: "incidentType" as const,
+      },
+      {
+        Header: "Officer(s) Involved",
+        accessor: "officersInvolved" as const,
+      },
+      {
+        Header: "Subject",
+        accessor: "subject" as const,
+      },
+      {
+        Header: "Source",
+        accessor: "source" as const,
+      },
     ],
     []
-);
+  )
+  const tableInstance = useTable({ columns, data })
 
-function DataTable() {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
 
-    const tableInstance = useTable({ columns, data });
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    );
-
+  return (
+    <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  borderBottom: "solid 3px red",
+                  background: "aliceblue",
+                  color: "black",
+                  fontWeight: "bold",
+                }}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    style={{
+                      padding: "10px",
+                      border: "solid 1px gray",
+                      background: "papayawhip",
+                    }}>
+                    {cell.render("Cell")}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
