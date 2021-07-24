43b791d5385aabc0703c7d8bd51426cf25afa501
michaelc <dylanesque27@gmail.com>
2021-07-24 17:07:26 -0400

    save changes

--- frontend/shared-components/data-table/data-table.tsx
+++ frontend/shared-components/data-table/data-table.tsx
@@ -97,21 +97,12 @@ export function DataTable() {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
 
   return (
-    <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+    <table {...getTableProps()}>
       <thead>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: "solid 3px red",
-                  background: "aliceblue",
-                  color: "black",
-                  fontWeight: "bold",
-                }}>
-                {column.render("Header")}
-              </th>
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
             ))}
           </tr>
         ))}
@@ -122,17 +113,7 @@ export function DataTable() {
           return (
             <tr {...row.getRowProps()}>
               {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: "10px",
-                      border: "solid 1px gray",
-                      background: "papayawhip",
-                    }}>
-                    {cell.render("Cell")}
-                  </td>
-                )
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
               })}
             </tr>
           )
