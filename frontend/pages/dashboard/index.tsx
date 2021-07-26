c537092ff794bd5945e6c3ba287f4729be398b7a
michaelc <dylanesque27@gmail.com>
2021-07-26 15:33:13 -0400

    Final style adjustments before first PR pass

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
 import { DataTable } from "../../shared-components/data-table/data-table"
+import styles from './dashboard.module.css'
 
 export default function Dashboard() {
   return (
-    <>
+    <div>
       <div className="Dashboard">I AM DASHBORD!!!!!!</div>
       <DataTable />
-    </>
+    </div>
   )
 }
--- frontend/shared-components/data-table/data-table.tsx
+++ frontend/shared-components/data-table/data-table.tsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGreaterThan, faPlusCircle } from "@fortawesome/free-solid-svg-icons"
 
 import styles from "./data-table.module.css"
-import { callbackify } from "util"
 
 type IncidentData = {
   dates: string
@@ -17,7 +16,7 @@ type IncidentData = {
 
 export function DataTable({ count = 220375 }) {
   // Defines table rows
-  const { dTable, dHeader, dFooter, dPagContainer } = styles
+  const { dTable, dHeader, dFooter, dPagContainer, dRowPage, dTableWrapper } = styles
   const [rowsShown, setRowsShown] = React.useState(7)
 
   // TODO: When this gets changed from mocking to fetching the data from an api call, append
@@ -129,44 +128,51 @@ export function DataTable({ count = 220375 }) {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
 
   return (
-    <table {...getTableProps()} className={dTable}>
-      <thead className={dHeader}>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                console.log(cell)
-                if (cell.column.id === "full" || cell.column.id === "save") {
-                  return (
-                    <td>
-                      <FontAwesomeIcon icon={cell.value} />
-                    </td>
-                  )
-                }
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-              })}
+    <div className={dTableWrapper}>
+      <table {...getTableProps()} className={dTable}>
+        <thead className={dHeader}>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-      <tfoot className={dFooter}>
-        <span>{count} records found</span>
-        <div className={dPagContainer}>
-          <p>
-            Show <span>{rowsShown}</span> rows
-          </p>
-        </div>
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  console.log(cell)
+                  if (cell.column.id === "full" || cell.column.id === "save") {
+                    return (
+                      <td>
+                        <FontAwesomeIcon icon={cell.value} />
+                      </td>
+                    )
+                  }
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot className={dFooter}>
+          <tr>
+            <td>{count.toLocaleString()} records found</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              Show <span className={dRowPage}>{rowsShown}</span> rows
+            </td>
+            <td>&lt; 1 of 31,482 &gt;</td>
+          </tr>
+        </tfoot>
+      </table>
+      </div>
   )
 }
