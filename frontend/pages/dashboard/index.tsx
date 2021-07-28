c66a0b3067c8202804d8d20e7d1738482de28fdc
michaelc <dylanesque27@gmail.com>
2021-07-28 12:31:08 -0400

    final adjustments before review

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 
 import { DataTable } from "../../shared-components/data-table/data-table"
 import DashboardHeader from "../../compositions/dashboardHeader/index"
-import Layout from '../../shared-components/layout/Layout';
+import Layout from '../../shared-components/layout/layout'
 
 export default function Dashboard() {
   return (
     <Layout>
       <DashboardHeader />
       <DataTable />
-
     </Layout>
   )
-    
+}
