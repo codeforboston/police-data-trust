38b864a43a8263247fbf9ef2e0b067ac12f1170f
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-05 00:36:32 -0400

    export response components

--- frontend/compositions/index.tsx
+++ frontend/compositions/index.tsx
@@ -1,13 +1,14 @@
 import DashboardHeader from "./dashboard-header/dashboard-header"
 import EnrollmentCallToAction from './enrollment-call-to-action/enrollment-call-to-action'
 import EnrollmentHeader from './enrollment-header/enrollment-header'
-import EnrollmentResponse from './enrollment-response/enrollment-response'
 import PasswordAid from './password-aid/password-aid'
+import { RegistrationResponse, PassportApplicationResponse } from './enrollment-response/enrollment-response'
 
 export { 
   DashboardHeader,
   EnrollmentCallToAction,
   EnrollmentHeader,
-  EnrollmentResponse,
+  PassportApplicationResponse,
+  RegistrationResponse,
   PasswordAid
 }
