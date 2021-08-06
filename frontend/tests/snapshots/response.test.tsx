93e26cf3b7be3cd90ce315c6a8856bcb8f0b48a1
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-06 13:38:51 -0400

    make snapshot test work

--- frontend/tests/snapshots/response.test.tsx
+++ frontend/tests/snapshots/response.test.tsx
@@ -1,34 +1,26 @@
 import * as React from 'react'
-import renderer, { act } from "react-test-renderer"
+import renderer from "react-test-renderer"
 import {
   RegistrationResponse,
   PassportApplicationResponse
 } from '../../compositions/enrollment-response/enrollment-response'
 
 it('renders Register:Success correctly', () => {
-  act(() => {
-    const tree = renderer.create(<RegistrationResponse isSuccess />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  const tree = renderer.create(<RegistrationResponse isSuccess />).toJSON()
+  expect(tree).toMatchSnapshot()
 }),
 
 it('renders Register:Fail correctly', () => {
-  act(() => {
-    const tree = renderer.create(<RegistrationResponse isSuccess={false} />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  const tree = renderer.create(<RegistrationResponse />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
 it('renders Passport:Success correctly', () => {
-  act(() => {
-    const tree = renderer.create(<PassportApplicationResponse isSuccess />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  const tree = renderer.create(<PassportApplicationResponse isSuccess />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
 it('renders Passport:Fail correctly', () => {
-  act(() => {
-    const tree = renderer.create(<PassportApplicationResponse isSuccess={false} />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  const tree = renderer.create(<PassportApplicationResponse />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
