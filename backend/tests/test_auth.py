1ed770e05b128b40419105ee3078510881098e16
Ryan Mahan <ryanmahan97@gmail.com>
2021-08-17 17:40:04 -0400

    add auth/test route test

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,5 +1,5 @@
 import pytest
-from backend.database import User
+from backend.database import User, UserRole
 from backend.auth import user_manager
 from flask_jwt_extended import decode_token
 
@@ -8,6 +8,7 @@ def example_user(db_session):
     user = User(
         email="test@email.com",
         password=user_manager.hash_password("my_password"),
+        role=UserRole.PUBLIC,
         first_name="first",
         last_name="last",
     )
@@ -70,3 +71,22 @@ def test_jwt(client, db_session, example_user):
 
     assert db_user.email == example_user.email
     assert res.status_code == 200
+
+def test_auth_test(client, example_user):
+    login_res = client.post(
+        'api/v1/auth/login',
+        json={
+            "email": example_user.email,
+            "password": "my_password"
+        }
+    )
+    print(login_res.json["access_token"])
+
+    test_res = client.get(
+        'api/v1/auth/test',
+        headers={
+            "Authorization": "Bearer {0}".format(login_res.json["access_token"])
+        }
+    )
+
+    assert test_res.status_code == 200
