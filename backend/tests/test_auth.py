868b488feb75b946bb9bf031b757e18b96928dd3
Ryan Mahan <ryanmahan97@gmail.com>
2021-08-17 17:25:08 -0400

    update tests

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -3,16 +3,27 @@ from backend.database import User
 from backend.auth import user_manager
 from flask_jwt_extended import decode_token
 
+@pytest.fixture
+def example_user(db_session):
+    user = User(
+        email="test@email.com",
+        password=user_manager.hash_password("my_password"),
+        first_name="first",
+        last_name="last",
+    )
+    db_session.add(user)
+    db_session.commit()
+    return user
 
 @pytest.mark.parametrize(
     ("email", "password", "expected_status_code"),
     [
-        ("test@email.com", "password", 200),
+        ("test@email.com", "my_password", 200),
         ("bad_email", "bad_password", 400),
         (None, None, 400),
     ],
 )
-def test_register(client, email, password, expected_status_code):
+def test_register(client, db_session, email, password, expected_status_code):
     res = client.post(
         "api/v1/auth/register",
         json={
@@ -20,27 +31,22 @@ def test_register(client, email, password, expected_status_code):
             "password": password,
         },
     )
+
+    db_user = db_session.query(User).filter(email == User.email).first()
+
+    assert (db_user == None) == (expected_status_code != 200)
     assert res.status_code == expected_status_code
 
 
 @pytest.mark.parametrize(
     ("password", "expected_status_code"),
-    [("password", 200), ("bad_password", 401), (None, 400)],
+    [("my_password", 200), ("bad_password", 401), (None, 400)],
 )
-def test_login(client, db_session, password, expected_status_code):
-    test_user = User(
-        email="test2@email.com",
-        password=user_manager.hash_password("password"),
-        first_name="first",
-        last_name="last",
-    )
-    db_session.add(test_user)
-    db_session.commit()
-
+def test_login(client, example_user, db_session, password, expected_status_code):
     res = client.post(
         "api/v1/auth/login",
         json={
-            "email": "test2@email.com",
+            "email": example_user.email,
             "password": password,
         },
     )
@@ -48,21 +54,12 @@ def test_login(client, db_session, password, expected_status_code):
     assert res.status_code == expected_status_code
 
 
-def test_jwt(client, db_session):
-    test_user = User(
-        email="test3@email.com",
-        password=user_manager.hash_password("password"),
-        first_name="first",
-        last_name="last",
-    )
-    db_session.add(test_user)
-    db_session.commit()
-
+def test_jwt(client, db_session, example_user):
     res = client.post(
         "api/v1/auth/login",
         json={
-            "email": "test3@email.com",
-            "password": "password",
+            "email": "test@email.com",
+            "password": "my_password",
         },
     )
 
@@ -71,5 +68,5 @@ def test_jwt(client, db_session):
     user_id = decode_token(res.json["access_token"])["sub"]
     db_user = db_session.query(User).filter(user_id == User.id).first()
 
-    assert db_user.email == test_user.email
+    assert db_user.email == example_user.email
     assert res.status_code == 200
