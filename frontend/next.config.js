623771e6a7700424ffd77a8bb1c0ae292e438ea6
Alex Ball <me@alexjball.com>
2021-08-20 18:30:27 -0400

    Configure eslint using strict presets

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en"
+  },
+  eslint: {
+    dirs: ["tests", "shared-components", "pages", "models", "helpers", "compositions"]
   }
 }
