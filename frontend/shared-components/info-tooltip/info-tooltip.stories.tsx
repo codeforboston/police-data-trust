cda7393cf71702ba5e4b9aa014e37eb6b86fcadc
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-31 16:28:39 -0400

    shared component stories

---
+++ frontend/shared-components/info-tooltip/info-tooltip.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { InfoTooltip } from '..';
+import { TooltipTypes } from '../../models';
+
+export default {
+  title: 'Shared Components/Info Tooltip',
+  component: InfoTooltip
+} as ComponentMeta<typeof InfoTooltip>
+
+const Template: ComponentStory<typeof InfoTooltip> = (args) => <InfoTooltip {...args} />
+
+export const ViewerTooltip = Template.bind({})
+ViewerTooltip.args = {
+  type: TooltipTypes.VIEWER
+}
+
+export const IncidentTooltip = Template.bind({})
+IncidentTooltip.args = {
+  type: TooltipTypes.INCIDENTS
+}
---
+++ frontend/shared-components/layout/layout.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Layout } from '..'
+
+export default {
+  title: 'Shared Components/Layout',
+  component: Layout
+} as ComponentMeta<typeof Layout>
+
+const Template: ComponentStory<typeof Layout> = (args) => <Layout {...args} />
+
+export const DefaultLayout = Template.bind({})
