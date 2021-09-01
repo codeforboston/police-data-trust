38b9c008bac5b4e392239e4516975d9d160f5297
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-31 21:05:55 -0400

    move enums/types to model

--- frontend/models/index.tsx
+++ frontend/models/index.tsx
@@ -1,15 +1,18 @@
-import { AppRoutes } from './app-routes'
-import { CallToActionTypes, enrollmentCallToActionText } from './enrollment-cta'
-import { tooltipContent, TooltipTypes } from './info-tooltip'
-import { LogoSizes } from './logo-sizes'
-import { passwordToggleViews } from './password-aid'
-import { PrimaryInputNames, primaryInputValidation } from './primary-input'
-import { states } from './state-select'
+import { AppRoutes } from "./app-routes"
+import { CallToActionTypes, enrollmentCallToActionText } from "./enrollment-cta"
+import { tooltipContent, TooltipTypes } from "./info-tooltip"
+import { LogoSizes } from "./logo-sizes"
+import { passwordToggleViews } from "./password-aid"
+import { PrimaryInputNames, primaryInputValidation } from "./primary-input"
+import { EnrollmentTypes, enrollmentMessage } from "./response-data"
+import { states } from "./state-select"
 
 export {
   AppRoutes,
   CallToActionTypes,
   enrollmentCallToActionText,
+  EnrollmentTypes,
+  enrollmentMessage,
   LogoSizes,
   passwordToggleViews,
   PrimaryInputNames,
