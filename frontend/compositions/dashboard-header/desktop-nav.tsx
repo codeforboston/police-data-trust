f80eaa2b020547fe084911a5c39f6d3ec2799ba3
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-02 08:21:22 -0400

    lift nav comps for outside use

--- frontend/compositions/dashboard-header/desktop-nav.tsx
+++ frontend/compositions/dashboard-header/desktop-nav.tsx
@@ -1,29 +1,41 @@
 import * as React from "react"
+import Link from "next/link"
 import styles from "./dashboard-header.module.css"
+import { AppRoutes } from "../../models"
 
-export default function DesktopNav() {
-  const [selected, setSelected] = React.useState("Search")
 
-  const handleNavChange = (e: React.MouseEvent<Element>): void => {
-    const value = (e.target as HTMLElement).innerText
-    setSelected(value)
-  }
+interface NavTypes {
+  loc: AppRoutes,
+  text: string
+}
 
-  const setClassName = (linkName: string): string => {
-    return linkName === selected ? styles.tab : ""
+const topNav: NavTypes[] = [
+  { loc: AppRoutes.DASHBOARD, text: 'Search' },
+  { loc: AppRoutes.PROFILE, text: 'Profile' },
+  { loc: AppRoutes.ABOUT, text: 'About' }
+]
+
+interface DesktopNavProps {
+  currentNav: string,
+  selectNav: Function
+}
+
+export default function DesktopNav({ currentNav, selectNav}: DesktopNavProps) {
+
+
+  const clsName = (loc: string): string => {
+    return loc === currentNav ? styles.tab : ""
   }
 
   return (
-    <ul className={styles.rightHeader} onClick={handleNavChange}>
-      <li className={setClassName("Search")}>
-        <a href="#">Search</a>
-      </li>
-      <li className={setClassName("Profile")}>
-        <a href="#">Profile</a>
-      </li>
-      <li className={setClassName("About")}>
-        <a href="#">About</a>
-      </li>
+    <ul className={styles.rightHeader}>
+      {topNav.map(itm => (
+        <li key={itm.loc} className={clsName(itm.loc)} onClick={() => selectNav(itm.loc)}>
+          <Link href={itm.loc}>
+            <a>{itm.text}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   )
 }
