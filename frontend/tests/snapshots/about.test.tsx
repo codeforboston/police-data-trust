99e1051ffcf9dfcc5bc3701d405adb8008b06d70
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-03 07:15:30 -0400

    snapshot tests

---
+++ frontend/tests/snapshots/about.test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import About from '../../pages/about'
+import renderer from 'react-test-renderer'
+
+it("randers About Page correctly", () => {
+  const tree = renderer.create(<About />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
---
+++ frontend/tests/snapshots/profile.test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Profile from '../../pages/profile'
+import renderer from 'react-test-renderer'
+
+it("randers Profile Page correctly", () => {
+  const tree = renderer.create(<Profile />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
