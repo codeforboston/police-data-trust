7b35fcd95c5c9798ac7489087b1f919a7420f1fa
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-03 03:46:17 -0400

    model stuff

--- frontend/models/profile.tsx
+++ frontend/models/profile.tsx
@@ -1,4 +1,3 @@
-
 export enum ProfileMenu {
   USER_INFO = 'info',
   PROFILE_TYPE = 'type',
@@ -12,9 +11,92 @@ interface MenuText {
 }
 
 
-export const menuContent: { [key in ProfileMenu]: MenuText } = {
-  [ProfileMenu.USER_INFO]: { item: ProfileMenu.USER_INFO, text: "User Information" },
-  [ProfileMenu.PROFILE_TYPE]: { item: ProfileMenu.PROFILE_TYPE, text: "Profile Type" },
-  [ProfileMenu.SAVED_RESULTS]: { item: ProfileMenu.SAVED_RESULTS, text: "Saved Results" },
-  [ProfileMenu.SAVED_SEARCHES]: { item: ProfileMenu.SAVED_SEARCHES, text: "Saved Searches" }
+export const menuContent: {
+  [key in ProfileMenu]: MenuText
+} = {
+  [ProfileMenu.USER_INFO]: {
+    item: ProfileMenu.USER_INFO,
+    text: "User Information"
+  },
+  [ProfileMenu.PROFILE_TYPE]: {
+    item: ProfileMenu.PROFILE_TYPE,
+    text: "Profile Type"
+  },
+  [ProfileMenu.SAVED_RESULTS]: {
+    item: ProfileMenu.SAVED_RESULTS,
+    text: "Saved Results"
+  },
+  [ProfileMenu.SAVED_SEARCHES]: {
+    item: ProfileMenu.SAVED_SEARCHES,
+    text: "Saved Searches"
+  }
+}
+
+
+export enum UserRoles {
+  NONE = 0,
+  PASSPORT = 1,
+  PUBLIC = 2,
+  CONTRIBUTOR = 3,
+  ADMIN = 4
+}
+
+export interface UserType {
+  id: number,
+  active: boolean,
+  firstName: string,
+  lastName: string,
+  email: string,
+  emailConfirmedAt?: Date,
+  phone: string,
+  pwHash: string,
+  role: UserRoles
+}
+
+export const emptyUser: UserType = {
+  id: 0,
+  active: false,
+  firstName: '',
+  lastName: '',
+  email: '',
+  pwHash: '',
+  phone: '',
+  role: UserRoles.NONE
+}
+
+
+interface ProfileTypeText {
+  title: string,
+  content: string,
+  linkText?: string,
+  linkPath?: string
+}
+
+export const profileTypeContent: { [key in UserRoles]: ProfileTypeText} = {
+  [UserRoles.NONE]: {
+    title: 'No Account',
+    content: '',
+    linkText: 'Sign up for an account',
+    linkPath: '/register'
+  },
+  [UserRoles.PUBLIC]: {
+    title: 'Viewer Account',
+    content: 'A Viewer account allows you to see data that is publicly available. To apply to see legally protected data...',
+    linkText: 'Register for a Passport Account',
+    linkPath: '/passport'
+  },
+  [UserRoles.PASSPORT]: {
+    title: 'Passport Account',
+    content: 'A Passport account allows you to see legally protected data. If the publicly available data is sufficient for your needs...',
+    linkText: 'Revert to a Viewer Account',
+    linkPath: '/register'
+  },
+  [UserRoles.CONTRIBUTOR]: {
+    title: 'Contributor Account',
+    content: ''
+  },
+  [UserRoles.ADMIN]: {
+    title: 'Administrator Account',
+    content: ''
+  }
 }
