bed927dae3c9ef71f315acd3efd0fc8b1332b422
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-03 03:45:54 -0400

    internal navigation

--- frontend/compositions/profile-nav/profile-nav.tsx
+++ frontend/compositions/profile-nav/profile-nav.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { ProfileMenu } from '../../models/profile' 
-import styles from './profile.module.css'
-
+import styles from './profile-nav.module.css'
 
 interface ProfileNavProps {
   currentItem: ProfileMenu,
@@ -18,14 +17,17 @@ const menuItems = [
 
 export default function ProfileNav({ currentItem, selectNav }: ProfileNavProps) {
 
-  const clsName = (itm: string) => itm === currentItem ? 'selected' : ''
+  const clsName = (itm: string) => itm === currentItem ? styles.selectedItem : styles.menuItem
 
   return (
     <nav className={styles.leftMenu}>
       <ul>
-        {menuItems.map(itm => (
-          <li key={itm.item} onClick={() => selectNav(itm.item)} className={clsName(itm.item)}>
-            {itm.text}
+        {menuItems.map(mnu => (
+          <li 
+            key={mnu.item} 
+            onClick={() => selectNav(mnu.item)} 
+            className={clsName(mnu.item)}>
+            {mnu.text}
           </li>
         ))}
       </ul>
