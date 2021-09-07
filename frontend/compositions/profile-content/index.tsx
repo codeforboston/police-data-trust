172c465300639618542a016267e26ca45c504242
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-07 16:49:14 -0400

    restructure

--- frontend/compositions/profile-content/index.tsx
+++ frontend/compositions/profile-content/index.tsx
@@ -1,40 +1,12 @@
-import * as React from 'react'
-import styles from './profile-content.module.css'
-import { UserProfileProps, emptyUser } from '../../models/profile'
 import ProfileInfo from './profile-info'
 import ProfileType from './profile-type'
-
-
-// placeholders
-const SavedSearch = ({ userData = emptyUser }: UserProfileProps) => {
-
-  return (
-    <div className={styles.savedData}>
-      <header className={styles.dataTitle}>
-        Saved Searches
-        <button>Edit Searches</button>
-      </header>
-    </div>
-    
-    )
-}
-
-const SavedResults = ({ userData = emptyUser }: UserProfileProps) => {
-  return (
-    <div className={styles.savedData}>
-      <header className={styles.dataTitle}>
-        Saved Results
-        <button>Edit Results</button>
-      </header>
-    </div>
-    
-    )
-}
+import SavedSearches from './saved-searches'
+import SavedResults from './saved-results'
 
 
 export {
   ProfileInfo,
   ProfileType,
   SavedResults,
-  SavedSearch
+  SavedSearches
 }
