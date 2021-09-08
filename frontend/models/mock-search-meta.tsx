fc8d1c34ede6fe20e683a4f5f19189256326d143
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-08 01:04:41 -0400

    only meta info

---
+++ frontend/models/mock-search-meta.tsx
@@ -0,0 +1,65 @@
+import { faCaretRight, faAngleRight, IconDefinition } from "@fortawesome/free-solid-svg-icons"
+
+export const resultsColumns = [
+  {
+    Header: "Search Date",
+    accessor: "searchDate" as const
+  },
+  {
+    Header: "Incident Date(s)",
+    accessor: "dates" as const
+  },
+  {
+    Header: "Incident Type",
+    accessor: "incidentType" as const
+  },
+  {
+    Header: "Officer(s)",
+    accessor: "officersInvolved" as const
+  },
+  {
+    Header: "Subject",
+    accessor: "subject" as const
+  },
+  {
+    Header: "Source",
+    accessor: "source" as const
+  },
+  {
+    Header: "View Record",
+    accessor: "full" as const
+  }
+]
+
+
+export const searchesColumns = [
+  {
+    Header: "Search Date",
+    accessor: "searchDate" as const
+  },
+  {
+    Header: "Who",
+    accessor: "who" as const
+  },
+  {
+    Header: "What",
+    accessor: "what" as const
+  },
+  {
+    Header: "When",
+    accessor: "when" as const
+  },
+  {
+    Header: "Where",
+    accessor: "where" as const
+  },
+  {
+    Header: "Results Total",
+    accessor: "results" as const
+  },
+  {
+    Header: "View Results",
+    accessor: "view" as const
+  }
+]
+
