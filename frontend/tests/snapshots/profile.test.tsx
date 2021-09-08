3f685bda455e8a6529f3ebf33d87cf1afab10fcf
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-08 01:06:43 -0400

    fix typo

--- frontend/tests/snapshots/profile.test.tsx
+++ frontend/tests/snapshots/profile.test.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Profile from "../../pages/profile"
 import renderer from "react-test-renderer"
 
-it("randers Profile Page correctly", () => {
+it("renders Profile Page correctly", () => {
   const tree = renderer.create(<Profile />).toJSON()
   expect(tree).toMatchSnapshot()
 })
