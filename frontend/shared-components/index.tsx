17711b62f84ef72d0abb71e7dadeedf44ee8df71
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-07 23:17:30 -0400

    add external link

--- frontend/shared-components/index.tsx
+++ frontend/shared-components/index.tsx
@@ -1,3 +1,4 @@
+import ExternalLink from "./external-link/external-link"
 import FormLevelError from "./error-form-level/error-form-level"
 import InfoTooltip from "./info-tooltip/info-tooltip"
 import Layout from "./layout/layout"
@@ -6,4 +7,4 @@ import PrimaryInput from "./primary-input/primary-input"
 import ResponseTextArea from "./response-textarea/response-textarea"
 import USAStateInput from "./state-input/state-input"
 
-export { FormLevelError, InfoTooltip, Layout, Logo, PrimaryInput, ResponseTextArea, USAStateInput }
+export { ExternalLink, FormLevelError, InfoTooltip, Layout, Logo, PrimaryInput, ResponseTextArea, USAStateInput }
