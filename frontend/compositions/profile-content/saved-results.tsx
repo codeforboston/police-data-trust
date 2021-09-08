92cabae788cfee6658d8d05ff94afbd14ef695ee
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-08 01:07:40 -0400

    use data-tables

--- frontend/compositions/profile-content/saved-results.tsx
+++ frontend/compositions/profile-content/saved-results.tsx
@@ -1,17 +1,140 @@
 import * as React from "react"
-import styles from "./saved.module.css"
+
+import { useTable, usePagination } from "react-table"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons"
+
 import { UserProfileProps, emptyUser } from "../../models/profile"
-import { DataTable } from "../../shared-components/data-table/data-table"
+import { resultsColumns } from "../../models/mock-search-meta"
+
+import styles from "./saved.module.css"
+import tableStyles from "../../shared-components/data-table/data-table.module.css"
+
+import mockData from "../../models/__mocks__/saved-results.json"
 
 export default function SavedResults({ userData = emptyUser }: UserProfileProps) {
-  const [editMode, setEditMode] = React.useState(false)
+  const { useState, useMemo } = React
+
+  const [editMode, setEditMode] = useState(false)
+  
   const { tableWrapper, tableTitle, editButton } = styles
+  const { dataTable, dataHeader, dataFooter, dataRowPage, dataRows } = tableStyles
+  
+  const data = useMemo(() => mockData, [])
+  const columns = useMemo(() => resultsColumns, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }
+    },
+    usePagination
+  )
+
+  const [pageSizeValue, setPageSizeValue] = useState(pageSize)
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPageSizeValue(+e.target.value)
+  }
+
+  function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    setPageSize(+e.target.value)
+  }
+
+  function viewRecord(recordId: number) {
+    // TODO: view full record
+  }
+
+  function toggleEditMode() {
+    setEditMode(!editMode)
+  }
+
   return (
     <div className={tableWrapper}>
       <header>
         <span className={tableTitle}>Saved Results</span>
-        <button className={editButton}>Edit Results</button>
+        <button className={editButton} onClick={toggleEditMode}>Edit Results</button>
       </header>
+      <table {...getTableProps()} className={dataTable} aria-label="Saved Results">
+        <thead className={dataHeader}>
+          {headerGroups.map((headerGroup) => (
+            // eslint-disable-next-line react/jsx-key
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // eslint-disable-next-line react/jsx-key
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <tr {...row.getRowProps()} className={dataRows}>
+                {row.cells.map((cell) => {
+                  const { id } = cell.column
+                  if (id === "full") {
+                    return (
+                    <td>
+                      <FontAwesomeIcon icon={faAngleRight } />
+                    </td>
+                    )
+                  }
+                  // eslint-disable-next-line react/jsx-key
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot className={dataFooter}>
+          <tr>
+            <td>{data.length.toLocaleString()} records found</td>
+            <td colSpan={4}></td>
+            <td>
+              Show{" "}
+              <input
+                className={dataRowPage}
+                min={1}
+                max={10}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="number"
+                value={pageSizeValue}
+              />{" "}
+              rows
+            </td>
+            <td>
+              {" "}
+              <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {"<"}
+              </button>{" "}
+              {pageIndex + 1} of {pageOptions.length}{" "}
+              <button onClick={() => nextPage()} disabled={!canNextPage}>
+                {">"}
+              </button>{" "}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   )
 }
--- frontend/compositions/profile-content/saved-searches.tsx
+++ frontend/compositions/profile-content/saved-searches.tsx
@@ -1,17 +1,143 @@
 import * as React from "react"
-import styles from "./saved.module.css"
+
+import { useTable, usePagination } from "react-table"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons"
+
 import { UserProfileProps, emptyUser } from "../../models/profile"
+import { searchesColumns } from "../../models/mock-search-meta"
+
+import styles from "./saved.module.css"
+import tableStyles from "../../shared-components/data-table/data-table.module.css"
+
+import mockData from "../../models/__mocks__/saved-search.json"
 
 // placeholders
 export default function SavedSearches({ userData = emptyUser }: UserProfileProps) {
-  const [editMode, setEditMode] = React.useState(false)
+  const { useState, useMemo } = React
+
+  const [editMode, setEditMode] = useState(false)
+  
   const { tableWrapper, tableTitle, editButton } = styles
+  const { dataTable, dataHeader, dataFooter, dataRowPage, dataRows } = tableStyles
+  
+  const data = useMemo(() => mockData, [])
+  const columns = useMemo(() => searchesColumns, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }
+    },
+    usePagination
+  )
+
+  const [pageSizeValue, setPageSizeValue] = useState(pageSize)
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPageSizeValue(+e.target.value)
+  }
+
+  function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    setPageSize(+e.target.value)
+  }
+
+  function viewRecord(recordId: number) {
+    // TODO: view full record
+  }
+
+  function toggleEditMode() {
+    setEditMode(!editMode)
+  }
+
   return (
-    <div className={tableWrapper}>
-      <header>
-        <span className={tableTitle}>Saved Searches</span>
-        <button className={editButton}>Edit Searches</button>
-      </header>
-    </div>
+
+      <div className={tableWrapper}>
+        <header>
+          <span className={tableTitle}>Saved Searches</span>
+          <button className={editButton} onClick={toggleEditMode}>Edit Searches</button>
+        </header>
+        <table {...getTableProps()} className={dataTable} aria-label="Saved Results">
+        <thead className={dataHeader}>
+          {headerGroups.map((headerGroup) => (
+            // eslint-disable-next-line react/jsx-key
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // eslint-disable-next-line react/jsx-key
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <tr {...row.getRowProps()} className={dataRows}>
+                {row.cells.map((cell) => {
+                  const { id } = cell.column
+                  if (id === "full") {
+                    return (
+                    <td>
+                      <FontAwesomeIcon icon={faCaretRight } />
+                    </td>
+                    )
+                  }
+                  // eslint-disable-next-line react/jsx-key
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                })}
+
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot className={dataFooter}>
+          <tr>
+            <td>{data.length.toLocaleString()} records found</td>
+            <td colSpan={4}></td>
+            <td>
+              Show{" "}
+              <input
+                className={dataRowPage}
+                min={1}
+                max={10}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="number"
+                value={pageSizeValue}
+              />{" "}
+              rows
+            </td>
+            <td>
+              {" "}
+              <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {"<"}
+              </button>{" "}
+              {pageIndex + 1} of {pageOptions.length}{" "}
+              <button onClick={() => nextPage()} disabled={!canNextPage}>
+                {">"}
+              </button>{" "}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+      </div>
   )
 }
