de9753f4b0fde63332635701015ff84acf2c66db
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-15 13:42:37 -0400

    snapshot tests

---
+++ frontend/tests/snapshots/profile-type.test.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { ProfileType } from "../../compositions/profile-content"
+import renderer from "react-test-renderer"
+import {users} from "../../models/mockData/"
+
+
+it("renders Profile: Account Type panel with regular Viewer user", () => {
+  const user = users[0]
+  const tree = renderer.create(<ProfileType userData={user} />)
+  expect(tree).toMatchSnapshot()
+})
+
+it("renders Profile: Account Type panel with Passport user", () => {
+  const user = users[1]
+  const tree = renderer.create(<ProfileType userData={user} />)
+  expect(tree).toMatchSnapshot()
+})
