509ec6003445a27676f3ff6121cf8a99a5999fac
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-15 01:46:32 -0400

    use PrimaryInput in form

--- frontend/compositions/profile-content/profile-info.tsx
+++ frontend/compositions/profile-content/profile-info.tsx
@@ -3,44 +3,46 @@ import styles from "./profile-content.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEdit } from "@fortawesome/free-solid-svg-icons"
 import { UserProfileProps, emptyUser } from "../../models/profile"
+import { PrimaryInputNames } from "../../models"
+import { PrimaryInput } from "../../shared-components"
 
 export default function ProfileInfo({ userData = emptyUser }: UserProfileProps) {
   const [editMode, setEditMode] = React.useState(false)
+  const [isSubmitted, setIsSubmitted] = React.useState(false)
+
+  const { FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, PHONE_NUMBER, CREATE_PASSWORD, CONFIRM_PASSWORD } = PrimaryInputNames
+  
   const { profileData, sectionTitle, row } = styles
   const { firstName, lastName, email, phone } = userData
 
-  function handleSubmit(e: React.FormEvent) {
-    // TODO: update user
+  function handleSubmit($event: React.FormEvent<HTMLButtonElement>) {
+    // TODO: validate input, update user
+    $event.preventDefault()
+    setIsSubmitted(true)
   }
 
   if (editMode) {
     return (
       <div className={profileData}>
         <header className={sectionTitle}>Edit Your Account Information</header>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className={row}>
-            <label htmlFor="firstName">First name:</label>
-            <input type="text" id="firstName" placeholder={firstName} />
-            <label htmlFor="lastName">Last name:</label>
-            <input type="text" id="lastName" placeholder={lastName} />
+            <PrimaryInput inputName={FIRST_NAME} isSubmitted={isSubmitted} />
+            <PrimaryInput inputName={LAST_NAME} isSubmitted={isSubmitted} />
           </div>
           <div className={row}>
-            <label htmlFor="email">Email address:</label>
-            <input type="email" id="email" placeholder={email} />
-            <label htmlFor="phone">Phone number:</label>
-            <input type="phone" id="phone" placeholder={phone} />
+            <PrimaryInput inputName={EMAIL_ADDRESS} isSubmitted={isSubmitted} />
+            <PrimaryInput inputName={PHONE_NUMBER} isSubmitted={isSubmitted} />
           </div>
           <div className={row}>
-            <label htmlFor="newPassword">Password:</label>
-            <input type="password" id="newPassword" />
-            <label htmlFor="confirmPassword">Confirm New Password</label>
-            <input type="password" id="confirmPassword" />
+            <PrimaryInput inputName={CREATE_PASSWORD} isSubmitted={isSubmitted} />
+            <PrimaryInput inputName={CONFIRM_PASSWORD} isSubmitted={isSubmitted} />
           </div>
           <div className={row}>
             <button className={styles.cancelButton} onClick={() => setEditMode(false)}>
               Cancel
             </button>
-            <input type="submit" className={styles.submitButton} value="Save Changes" />
+            <button className={styles.submitButton} onClick={handleSubmit}>Save Changes</button>
           </div>
         </form>
       </div>
@@ -70,10 +72,7 @@ export default function ProfileInfo({ userData = emptyUser }: UserProfileProps) 
           <span className={label}>Phone number:</span>
           <div className={dataField}>{phone}</div>
         </div>
-        <div className={row}>
-          <span className={label}>Password:</span>
-          <div className={dataField}>********</div>
-        </div>
+        
       </div>
     )
   }
