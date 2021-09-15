f9ad85d64e32cfdf00431c0fef9e8d444c6d5785
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-15 01:47:26 -0400

    index for mock data

---
+++ frontend/models/mockData/index.ts
@@ -0,0 +1,5 @@
+import * as savedResults from "./saved-results.json"
+import * as savedSearches from "./saved-search.json"
+import * as users from "./users.json"
+
+export { savedResults, savedSearches, users }
