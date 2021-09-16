38ecfdbc470ff4a1ce263b3823c56596725ab019
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-16 14:40:23 -0400

    move mock data

--- frontend/models/mockData/index.ts
+++
@@ -1,5 +0,0 @@
-import * as savedResults from "./saved-results.json"
-import * as savedSearches from "./saved-search.json"
-import * as users from "./users.json"
-
-export { savedResults, savedSearches, users }
