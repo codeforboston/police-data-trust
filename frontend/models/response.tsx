8526c4f5d40d3081bada587c03cb2d24bfa12401
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-17 05:24:37 -0400

    add success response message

--- frontend/models/response.tsx
+++ frontend/models/response.tsx
@@ -23,3 +23,33 @@ export const enrollmentMessage: { [key in EnrollmentTypes]: EnrollmentErrorText 
     returnPath: AppRoutes.DASHBOARD
   }
 }
+
+interface EnrollmentResponseText {
+  title: string
+  message: string[]
+  returnLink?: string
+  returnText?: string
+}
+
+// TODO: specify how many is XX days!
+export const successResponseMessage: { [key in EnrollmentTypes]: EnrollmentResponseText } = {
+  [EnrollmentTypes.VIEWER]: {
+    title: "Success!",
+    message: [
+      "You have been successfully registered as a Viewer",
+      "**Please check your email to confirm your registration**",
+      "The confirmation e-mail will direct you to a new login screen",
+      "**If you need access to legally protected data,** you are also invited to apply for a **Passport Account** upon log in."
+    ]
+  },
+  [EnrollmentTypes.PASSPORT]: {
+    title: "Success!",
+    message: [
+      "You have successfully submitted an application for a Passport account",
+      "**Please check your email over the next few days**",
+      "You can expect to receive a decision in XX days with further instructions. In the meantime, you may continue to explore all public data."
+    ],
+    returnLink: AppRoutes.DASHBOARD,
+    returnText: "Return to dashboard"
+  }
+}
