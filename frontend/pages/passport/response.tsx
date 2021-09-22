b50afc97906490c2e639bd9416a245a4225c312f
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-21 20:50:58 -0400

    simplify response demo pages

--- frontend/pages/passport/response.tsx
+++ frontend/pages/passport/response.tsx
@@ -1,19 +1,16 @@
 import * as React from "react"
-import { Layout } from "../../shared-components"
-import { PassportApplicationResponse, ResponseProps } from "../../compositions/enrollment-response/enrollment-response"
+import { PassportApplicationResponse } from "../../compositions/enrollment-response/enrollment-response"
 
-export default function Response(props: ResponseProps) {
+export default function Response() {
   const wrapper = {
     width: "60%",
     margin: "20vh auto"
   }
 
   return (
-    <Layout>
-      <div style={wrapper}>
-        <PassportApplicationResponse {...props} />
-      </div>
-      
-    </Layout>
+    <div style={wrapper}>
+      <PassportApplicationResponse success={true} />
+      <PassportApplicationResponse success={false} />
+    </div>
   )
 }
--- frontend/pages/register/response.tsx
+++ frontend/pages/register/response.tsx
@@ -1,17 +1,16 @@
 import * as React from "react"
 import { Layout } from "../../shared-components"
-import { RegistrationResponse, ResponseProps } from "../../compositions/enrollment-response/enrollment-response"
+import { RegistrationResponse } from "../../compositions/enrollment-response/enrollment-response"
 
-export default function Response(props: ResponseProps) {
+export default function Response() {
   const wrapper = {
     width: "60%",
     margin: "20vh auto"
   }
   return (
-    <Layout>
-      <div style={wrapper}>
-        <RegistrationResponse {...props} />
-      </div>
-    </Layout>
+    <div style={wrapper}>
+      <RegistrationResponse success={true} />
+      <RegistrationResponse success={false} />
+    </div>
   )
 }
