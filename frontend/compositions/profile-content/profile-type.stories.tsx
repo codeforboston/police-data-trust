6fa3a7e99631fdec9db574cdbee30a876c3af302
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 14:35:23 -0400

    profile stories

---
+++ frontend/compositions/profile-content/profile-type.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ProfileType } from "."
+import users from "../../models/mock-data/users.json"
+
+export default {
+  title: "Compositions/Profile Type",
+  component: ProfileType
+} as ComponentMeta<typeof ProfileType>
+
+const Template: ComponentStory<typeof ProfileType> = (args) => <ProfileType {...args} />
+
+export const PublicProfile = Template.bind({})
+PublicProfile.args = {
+  userData: users[0]
+}
+
+export const PassportProfile = Template.bind({})
+PassportProfile.args = {
+  userData: users[1]
+}
---
+++ frontend/compositions/profile-nav/profile-nav.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import ProfileNav from "./profile-nav"
+
+export default {
+  title: "Compositions/Profile Nav",
+  component: ProfileNav
+} as ComponentMeta<typeof ProfileNav>
+
+const Template: ComponentStory<typeof ProfileNav> = (args) => <ProfileNav {...args} />
+
+export const ProfileNavMenu = Template.bind({})
+ProfileNavMenu.args = {}
