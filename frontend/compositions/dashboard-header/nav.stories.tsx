4b96d56a4c2711a1093d9f11729c2bb9250373a4
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 14:34:04 -0400

    nav story

---
+++ frontend/compositions/dashboard-header/nav.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Nav from './nav'
+
+export default {
+  title: 'Compositions/Desktop Nav',
+  component: Nav
+} as ComponentMeta<typeof Nav>
+
+const Template: ComponentStory<typeof Nav> = (args) => <Nav {...args} />
+
+export const DesktopNav = Template.bind({})
+DesktopNav.args = {}
+
