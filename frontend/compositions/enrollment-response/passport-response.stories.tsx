8d5896acc1c43c0b2ea34440653bbc0e04e3cfcf
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 14:34:47 -0400

    enrollment response stories

---
+++ frontend/compositions/enrollment-response/passport-response.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { PassportApplicationResponse } from '..'
+
+export default {
+  title: 'Compositions/Passport Response',
+  component: PassportApplicationResponse
+} as ComponentMeta<typeof PassportApplicationResponse>
+
+const Template: ComponentStory<typeof PassportApplicationResponse> = (args) => <PassportApplicationResponse {...args} />
+
+export const PassportSuccess = Template.bind({})
+PassportSuccess.args = {
+  success: true
+}
+
+export const PassportFailure = Template.bind({})
+PassportFailure.args = {
+  success: false
+}
