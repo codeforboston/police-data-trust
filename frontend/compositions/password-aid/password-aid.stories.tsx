3235715948f123027171f5dc9bf0f88b48f039d9
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 14:35:07 -0400

    password aid story

---
+++ frontend/compositions/password-aid/password-aid.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { PasswordAid } from '..'
+
+export default {
+  title: 'Compositions/Password Aid',
+  component: PasswordAid
+} as ComponentMeta<typeof PasswordAid>
+
+const Template: ComponentStory<typeof PasswordAid> = (args) => <PasswordAid {...args} />
+
+export const ShowPassword = Template.bind({})
+ShowPassword.args = {
+  
+}
