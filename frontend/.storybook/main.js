08b89a5a877272f4852fa9538bcaab1c145dec40
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 21:40:08 -0400

    next router

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,4 +1,13 @@
 module.exports = {
-  stories: ["../compositions/**/*.stories.tsx", "../shared-components/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "storybook-css-modules-preset"]
+  stories: [
+    "../compositions/**/*.stories.tsx",
+    "../shared-components/**/*.stories.tsx",
+    "../pages/**/*.stories.tsx"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+    "storybook-addon-next-router"
+  ]
 }
