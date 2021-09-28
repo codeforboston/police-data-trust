c3a57fb9c1eed34f7990e1971cebb3b933be136b
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 21:39:48 -0400

    next router context

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { RouterContext } from "next/dist/shared/lib/router-context"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +7,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider
   }
 }
