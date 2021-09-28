6c33556abbe71ab602390f52920399cd87213fca
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-27 21:39:21 -0400

    combine into one story

---
+++ frontend/compositions/enrollment-response/enrollment-response.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { PassportApplicationResponse, RegistrationResponse } from ".."
+
+export default {
+  title: "Compositions/Enrollment Response",
+  component: PassportApplicationResponse
+} as ComponentMeta<typeof PassportApplicationResponse>
+
+const PassportTemplate: ComponentStory<typeof PassportApplicationResponse> = (args) => (
+  <PassportApplicationResponse {...args} />
+)
+
+const RegisterTemplate: ComponentStory<typeof RegistrationResponse> = (args) => (
+  <RegistrationResponse {...args} />
+)
+
+export const PassportResponse = PassportTemplate.bind({})
+PassportResponse.args = {
+  success: true
+}
+
+export const RegistraterResponse  = RegisterTemplate.bind({})
+RegistraterResponse.args = {
+  success: true
+}
