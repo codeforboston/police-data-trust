a39a0d042ad7a061f0286799869c5403fce0233a
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-29 20:13:12 -0400

    add msw decorator

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -2,8 +2,15 @@ import { RouterContext } from "next/dist/shared/lib/router-context"
 // import { addDecorator } from "@storybook/react"
 // import { withContexts } from "@storybook/addon-contexts/react"
 // import { contexts } from "./contexts"
+import { worker } from "../helpers/api/mocks/browser"
+import FakeAuth from "../helpers/api/mocks/fake-auth"
 
 // addDecorator(withContexts(contexts))
+
+if (typeof global.process === "undefined") {
+  worker.start()
+  // FakeAuth.start()
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
