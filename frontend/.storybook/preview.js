ca3496f9ac69016d99c9cd5e8b4155ad2ecd5587
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-30 13:28:57 -0400

    remove (probably) unnecessary parts

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,15 +1,8 @@
 import { RouterContext } from "next/dist/shared/lib/router-context"
-// import { addDecorator } from "@storybook/react"
-// import { withContexts } from "@storybook/addon-contexts/react"
-// import { contexts } from "./contexts"
-import { worker } from "../helpers/api/mocks/browser"
-import FakeAuth from "../helpers/api/mocks/fake-auth"
 
-// addDecorator(withContexts(contexts))
-
-if (typeof global.process === "undefined") {
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("../helpers/api/mocks/browser")
   worker.start()
-  // FakeAuth.start()
 }
 
 export const parameters = {
