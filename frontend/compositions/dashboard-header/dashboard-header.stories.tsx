10c0d8e1e05d35845a7ad38914756c8ba1db3499
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-30 13:30:02 -0400

    add AuthProvider decorator

--- frontend/compositions/dashboard-header/dashboard-header.stories.tsx
+++ frontend/compositions/dashboard-header/dashboard-header.stories.tsx
@@ -1,10 +1,18 @@
 import React from "react"
 import { ComponentStory, ComponentMeta } from "@storybook/react"
 import { DashboardHeader } from ".."
+import { AuthProvider } from "../../helpers"
 
 export default {
   title: "Compositions/Header",
-  component: DashboardHeader
+  component: DashboardHeader,
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    )
+  ]
 } as ComponentMeta<typeof DashboardHeader>
 
 const Template: ComponentStory<typeof DashboardHeader> = (args) => <DashboardHeader {...args} />
--- frontend/pages/login/login.stories.tsx
+++ frontend/pages/login/login.stories.tsx
@@ -1,10 +1,18 @@
 import React from "react"
 import { Meta, Story } from "@storybook/react"
 import UserLogin from "./"
+import { AuthProvider } from "../../helpers"
 
 export default {
   title: "Pages/Login",
-  component: UserLogin
+  component: UserLogin,
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    )
+  ]
 } as Meta<typeof UserLogin>
 
 const Template: Story<typeof UserLogin> = (args) => <UserLogin {...args} />
