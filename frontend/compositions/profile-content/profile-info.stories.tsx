e448f14647783104f740695c2c4c12c5665bf1a7
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-30 21:25:46 -0400

    add context provider

--- frontend/compositions/profile-content/profile-info.stories.tsx
+++ frontend/compositions/profile-content/profile-info.stories.tsx
@@ -1,11 +1,23 @@
 import React from "react"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
 import { ProfileInfo } from "."
+import { FormProvider, useForm } from "react-hook-form"
 import users from "../../models/mock-data/users.json"
+
 
 export default {
   title: "Compositions/Profile Info",
-  component: ProfileInfo
+  component: ProfileInfo,
+  decorators: [
+    (Story) => {
+      const methods = useForm()
+      return (
+        <FormProvider {...methods}>
+          <Story />
+        </FormProvider>
+      )
+    }
+  ]
 } as ComponentMeta<typeof ProfileInfo>
 
 const Template: ComponentStory<typeof ProfileInfo> = (args) => <ProfileInfo {...args} />
