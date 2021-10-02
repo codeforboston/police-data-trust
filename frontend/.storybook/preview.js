efef0b69b80ff6ed1fbad43b50a1a7fc4839d35e
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-01 23:01:01 -0400

    use Providers for decorator

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,17 +1,10 @@
 import { RouterContext } from "next/dist/shared/lib/router-context"
-// import { FormProvider, useForm } from "react-hook-form"
-// import { AuthProvider } from "../helpers"
-// import { addDecorator } from "@storybook/react"
-// import { initializeWorker, mswDecorator } from "msw-storybook-addon"
+import { Providers } from "../helpers"
+import { addDecorator } from "@storybook/react"
+import { initialize, mswDecorator } from "msw-storybook-addon"
 
-// initializeWorker()
-// addDecorator(mswDecorator)
-
-// don't run in production or during build
-// if (process.env.NODE_ENV === "development" && typeof global.process === 'undefined') {
-//   const { worker } = require("../helpers/api/mocks/browser")
-//   worker.start()
-// }
+initialize()
+addDecorator(mswDecorator)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -21,18 +14,13 @@ export const parameters = {
       date: /Date$/
     }
   },
-  // decorators: [
-  //   (Story) => {
-  //     const methods = useForm()
-  //     return (
-  //       <AuthProvider>
-  //         <FormProvider {...methods}>
-  //           <Story />
-  //         </FormProvider>
-  //       </AuthProvider>
-  //     )
-  //   }
-  // ],
+  decorators: [
+    (Story) => (
+      <Providers>
+        <Story />
+      </Providers>
+    )
+  ],
   nextRouter: {
     Provider: RouterContext.Provider
   }
