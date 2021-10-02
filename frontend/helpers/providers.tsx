9668e16cffda1f9bfb714b95c0b3db8d08e59c56
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-01 22:57:25 -0400

    add FormProvider, saves typing

--- frontend/helpers/providers.tsx
+++ frontend/helpers/providers.tsx
@@ -1,7 +1,14 @@
 import { AuthProvider } from "./auth"
+import { FormProvider, useForm } from "react-hook-form"
 
 /**
  * Wraps components in application providers, which set up contexts to provide
  * services to components.
  */
-export const Providers: React.FC = ({ children }) => <AuthProvider>{children}</AuthProvider>
+export const Providers: React.FC = ({ children }) => (
+  <AuthProvider>
+    <FormProvider {...useForm()}>
+      {children}
+    </FormProvider>
+  </AuthProvider>
+)
