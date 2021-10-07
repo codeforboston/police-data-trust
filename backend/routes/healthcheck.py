a4abf5ef5a5998579f01b4fcc2e82664cd6981e9
Darren Pham <phamdarren1@gmail.com>
2021-10-07 00:10:31 -0700

    satisfy python tests

--- backend/routes/healthcheck.py
+++ backend/routes/healthcheck.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 bp = Blueprint("healthcheck", __name__,)
 
+
 # TODO: Add checks to validate backing services or dependencies
 @bp.route("/healthcheck", methods=["GET"])
 def healthcheck():
