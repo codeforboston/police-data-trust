62e17d57931e7a46e2e3574320c11d38d9305b26
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 17:43:44 -0400

    create Officer datatypes

---
+++ frontend/models/officer.ts
@@ -0,0 +1,55 @@
+import { IncidentTableData } from "./incidents";
+
+export interface DepartmentType {
+  departmentName: string,
+  deptImage: string,
+  deptAddress: string,
+  webAddress: string
+}
+
+export const departmentColumns = [
+  {
+    Header: "Department",
+    accessor: "dept"
+  },
+  {
+    Header: "Address",
+    accessor: "deptAddress"
+  },
+  {
+    Header: "Website",
+    accessor: "webAddress"
+  },
+  {
+    Header: "Dept Address",
+    accessor: "deptAddress"
+  }
+]
+
+
+export interface EmploymentType {
+  department: DepartmentType,
+  status: string,
+  startDate: Date,
+  endDate: Date,
+}
+
+
+
+
+export interface OfficerRecordType {
+  recordId: number,
+  firstName: string,
+  lastName: string,
+  badgeNo: string,
+  status: string,
+  department: string,
+  birthDate?: Date,
+  gender?: string,
+  race?: string,
+  ethnicity?: string,
+  incomeBracket?: string,
+  workHistory: EmploymentType[],
+  affiliations?: string[],
+  incidents?: IncidentTableData[]
+}
