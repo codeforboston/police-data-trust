4fadfe03b496e42e560b1c2c073b0a0cdf3de752
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 21:46:34 -0400

    replace results table component

--- frontend/pages/search/index.tsx
+++ frontend/pages/search/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { BubbleChart, DashboardHeader, Map } from "../../compositions"
 import { requireAuth } from "../../helpers"
 import { Layout } from "../../shared-components"
-import { DataTable } from "../../shared-components/data-table/data-table"
+import { SearchResultsTable } from "../../compositions"
 
 type ChartType = "bubble" | "map"
 
@@ -34,7 +34,7 @@ export default requireAuth(function Dashboard() {
         {VisChoiceButton("map")}
       </div>
       {whichChart === "map" ? <Map /> : <BubbleChart />}
-      <DataTable />
+      <SearchResultsTable />
     </Layout>
   )
 })
