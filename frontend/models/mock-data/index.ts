a1067b7187f5198f9ec9bde1d5eee52797e6bcab
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 21:38:02 -0400

    export mock data

--- frontend/models/mock-data/index.ts
+++ frontend/models/mock-data/index.ts
@@ -1,18 +1,13 @@
-import { mockData, tableColumns } from "./mock-table-data"
-import { savedResultsData } from "./saved-results-data"
-import { savedSearchData } from "./saved-search-data"
-import users from "./users.json"
-import mockSaved from "./saved-results.json"
 import mockIncident from "./incidents.json"
 import mockIncidentG from "./grammy.json"
+import mockSavedResults from "./saved-results.json"
+import mockSavedSearches from "./saved-searches.json"
+import mockUsers from "./users.json"
 
 export {
-  mockData,
   mockIncident,
   mockIncidentG,
-  mockSaved,
-  savedResultsData,
-  savedSearchData,
-  tableColumns,
-  users
+  mockSavedResults,
+  mockSavedSearches,
+  mockUsers
 }
