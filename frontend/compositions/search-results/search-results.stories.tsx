1347b605da1b62a5bde8d73a6d8618862fe1fc39
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 21:45:23 -0400

    search results component

---
+++ frontend/compositions/search-results/search-results.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import SearchResultsTable from "./"
+// import { IncidentTableData } from "../../models"
+
+export default {
+  title: "Compositions/Search Results Table",
+  component: SearchResultsTable
+} as ComponentMeta<typeof SearchResultsTable>
+
+const Template: ComponentStory<typeof SearchResultsTable> = (args) => <SearchResultsTable {...args} />
+
+export const Incidents = Template.bind({})
+Incidents.args = {
+  incidents: require("../../models/mock-data/incidents.json")
+}
+
+
+export const Grammies = Template.bind({})
+Grammies.args = {
+  incidents: require("../../models/mock-data/grammy.json")
+}
