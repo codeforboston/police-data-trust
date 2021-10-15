034208b3d93a4fe7250276874c28482a5153d9bd
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 21:42:09 -0400

    make mock-data optional

--- frontend/compositions/profile-saved-tables/saved-results.tsx
+++ frontend/compositions/profile-saved-tables/saved-results.tsx
@@ -1,17 +1,20 @@
 import SavedTable from "./saved-table"
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons"
 
-import { resultsColumns } from "../../models/search-meta"
-import { savedResultsData } from "../../models/mock-data"
+import { SavedResultsType, resultsColumns } from "../../models/saved-table"
+import { mockSavedResults } from "../../models/mock-data"
 
-// TODO: retrieve user saved results from API
 
-export default function SavedResults() {
+interface SavedResultsProps {
+  data?: Array<SavedResultsType>
+}
+
+export default function SavedResults({ data = mockSavedResults }: SavedResultsProps) {
   return SavedTable({
     itemTitle: "Results",
     tableColumns: resultsColumns,
-    tableData: savedResultsData,
-    rowIdName: "recordId",
+    tableData: data,
+    rowIdName: "id",
     expandIcon: faAngleRight
   })
 }
--- frontend/compositions/profile-saved-tables/saved-searches.tsx
+++ frontend/compositions/profile-saved-tables/saved-searches.tsx
@@ -1,16 +1,21 @@
 import SavedTable from "./saved-table"
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons"
 
-import { searchesColumns } from "../../models/search-meta"
-import { savedSearchData } from "../../models/mock-data"
+import { searchesColumns, SavedSearchType } from "../../models/saved-table"
+import { mockSavedSearches } from "../../models/mock-data"
+
 
 // TODO: retrieve user saved searches from API
 
-export default function SavedSearches() {
+interface SavedSearchProps {
+  data?: Array<SavedSearchType>
+}
+
+export default function SavedSearches({ data = mockSavedSearches }: SavedSearchProps) {
   return SavedTable({
     itemTitle: "Searches",
     tableColumns: searchesColumns,
-    tableData: savedSearchData,
+    tableData: data,
     rowIdName: "results",
     expandIcon: faCaretRight
   })
