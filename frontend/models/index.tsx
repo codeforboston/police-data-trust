22342ab0a158c123ea8c09987c53cbc1ae22eefa
Charlie Kelly <charliekelly31415@gmail.com>
2021-10-14 21:46:14 -0400

    export  files

--- frontend/models/index.tsx
+++ frontend/models/index.tsx
@@ -1,7 +1,9 @@
 import { AppRoutes } from "./app-routes"
 import { CallToActionTypes, enrollmentCallToActionText } from "./enrollment-cta"
 import { enrollmentMessage, EnrollmentTypes } from "./response"
-import { tooltipContent, TooltipTypes } from "./info-tooltip"
+import { IncidentTableData, IncidentDetailType } from "./incidents"
+import { SavedResultsType, SavedSearchType } from "./saved-table"
+import { tooltipContent, TooltipTypes, TooltipIcons } from "./info-tooltip"
 import { LogoSizes } from "./logo-sizes"
 import { passwordToggleViews } from "./password-aid"
 import { PrimaryInputNames, primaryInputValidation } from "./primary-input"
@@ -23,6 +25,7 @@ export {
   tooltipContent,
   TooltipTypes,
   SearchResultsTypes,
-  alertContent
+  alertContent,
+  TooltipIcons
 }
-export type { GeoJson, Filter, Data, CityProperties }
+export type { GeoJson, Filter, Data, CityProperties, IncidentTableData, IncidentDetailType, SavedResultsType, SavedSearchType }
