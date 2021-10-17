76d78378df12f4e7fefb12a7894d22bfd00d70e4
Alex Ball <me@alexjball.com>
2021-10-17 10:17:31 -0400

    Create example users for all roles

--- alembic/dev_seeds.py
+++ alembic/dev_seeds.py
@@ -2,17 +2,52 @@ from backend.database.core import db
 from backend.database import User, UserRole
 from backend.auth import user_manager
 
-user = User(
-    email="test@example.com",
-    password=user_manager.hash_password("password"),
-    role=UserRole.PUBLIC,
-    first_name="Test",
-    last_name="Example",
+
+def create_user(user):
+    user_exists = (
+        db.session.query(User).filter_by(email=user.email).first() is not None
+    )
+
+    if not user_exists:
+        user.create()
+
+
+create_user(
+    User(
+        email="test@example.com",
+        password=user_manager.hash_password("password"),
+        role=UserRole.PUBLIC,
+        first_name="Test",
+        last_name="Example",
+    )
 )
 
-user_exists = (
-    db.session.query(User).filter_by(email=user.email).first() is not None
+create_user(
+    User(
+        email="contributor@example.com",
+        password=user_manager.hash_password("password"),
+        role=UserRole.CONTRIBUTOR,
+        first_name="Contributor",
+        last_name="Example",
+    )
 )
 
-if not user_exists:
-    user.create()
+create_user(
+    User(
+        email="admin@example.com",
+        password=user_manager.hash_password("password"),
+        role=UserRole.ADMIN,
+        first_name="Admin",
+        last_name="Example",
+    )
+)
+
+create_user(
+    User(
+        email="passport@example.com",
+        password=user_manager.hash_password("password"),
+        role=UserRole.PASSPORT,
+        first_name="Passport",
+        last_name="Example",
+    )
+)
