7911002c92c0558f8ea01cd5289ea23b8c19e4c7
Alex Ball <me@alexjball.com>
2021-10-25 12:49:38 -0400

    Fix jest tests

--- frontend/compositions/dashboard-header/dashboard-header.tsx
+++ frontend/compositions/dashboard-header/dashboard-header.tsx
@@ -1,14 +1,16 @@
 import * as React from "react"
 import useDropdownMenu from "react-accessible-dropdown-menu-hook"
-import { Logo as NPDCLogo, PrimaryButton } from "../../shared-components"
-import Nav from "./nav"
-import styles from "./dashboard-header.module.css"
+import { useMediaQuery } from "react-responsive"
 import { LogoSizes } from "../../models"
+import { Logo as NPDCLogo } from "../../shared-components"
+import styles from "./dashboard-header.module.css"
 import MobileDropdown from "./mobile-dropdown"
+import Nav from "./nav"
 
 export default function DashboardHeader() {
   const { wrapper, backgroundBanner, leftHeader, titleContainer } = styles
   const { buttonProps, itemProps, isOpen } = useDropdownMenu(4)
+  const mobile = useMediaQuery({ query: "screen and (max-width: 32em)" })
 
   return (
     <header className={wrapper}>
@@ -21,8 +23,11 @@ export default function DashboardHeader() {
           </div>
         </div>
         <nav aria-label="Main Navigation">
-          <MobileDropdown itemProps={itemProps} buttonProps={buttonProps} isOpen={isOpen} />
-          <Nav itemProps={itemProps} />
+          {mobile ? (
+            <MobileDropdown itemProps={itemProps} buttonProps={buttonProps} isOpen={isOpen} />
+          ) : (
+            <Nav itemProps={itemProps} />
+          )}
         </nav>
       </div>
     </header>
