22cf9800310cf699330435c8985ebd0c836b7f6a
Alex Ball <me@alexjball.com>
2021-11-04 09:53:03 -0400

    test healthcheck endpoint

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.parametrize(
     ("page", "expected_status_code"),
-    [("/", 200)],
+    [("/", 200), ("/api/v1/healthcheck", 200)],
 )
 def test_simple_routes(client, page, expected_status_code):
     assert client.get(page).status_code == expected_status_code
