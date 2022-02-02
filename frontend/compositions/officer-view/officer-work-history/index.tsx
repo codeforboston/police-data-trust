6870ab1245a3d92019cf8fc575db36ea47fc9fff
SadPhantom1 <logandang100@yahoo.com>
2022-02-02 13:55:05 -0800

    Create work history component

---
+++ frontend/compositions/officer-view/officer-work-history/index.tsx
@@ -0,0 +1,25 @@
+import { OfficerRecordType } from "../../../models/officer"
+import WorkHistoryInstance from "./work-history-instance"
+import styles from "./officer-work-history.module.css"
+
+export default function OfficerWorkHistory(officer: OfficerRecordType) {
+  const { workHistory } = officer
+  const { category, workInstanceBlock } = styles
+  
+  let result : JSX.Element[] = []
+  for (const workInstance of workHistory) {
+    result.push(<WorkHistoryInstance {...workInstance} />)
+  }
+  
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div className={category}>
+        <p>Work History Summary:</p>
+      </div>
+      <div>
+        {result}
+      </div>
+    </div>
+  )
+}
