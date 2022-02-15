03153cc48fb92d9039b9226a01661d8bed7a8cd8
Logan-Dang <logandang100@yahoo.com>
2022-02-15 11:30:19 -0800

    Create Incident View Header

---
+++ frontend/compositions/incident-view/incident-view-header/incident-view-header.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta} from '@storybook/react'
+import IncidentViewHeader from '.'
+import incidents from '../../../models/mock-data/incidents.json'
+
+export default {
+    title: "Compositions/Incident View Components/IncidentHeader"
+} as ComponentMeta<typeof IncidentViewHeader>
+
+const Template: ComponentStory<typeof IncidentViewHeader> = (args) => <IncidentViewHeader {...args} />
+
+export const Blank = Template.bind({})
+
+export const Timothy = Template.bind({})
+Timothy.args = incidents[0]
