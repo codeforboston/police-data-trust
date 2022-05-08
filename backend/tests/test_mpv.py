e10030f354b7928cb5406b5e308afcab1d7184ad
ren0nie0 <32780767+ren0nie0@users.noreply.github.com>
2022-05-08 09:54:28 -0400

    MPV tests / Dockerfile updates
    -Created MPV tests
    -Fixed Dockerfile
    -Fixed Setup.cfg

---
+++ backend/tests/test_mpv.py
@@ -0,0 +1,51 @@
+import pytest
+from backend.scraper.mpv_2 import *
+
+test_dataset = {"Original_Column_Name": "test"}
+test_df = pd.DataFrame([test_dataset])
+test_m = {"Original_Column_Name": "target_column_name",}
+
+def test_map_cols():
+    result = map_cols(test_df, test_m)
+    assert  result.columns == "target_column_name"
+
+def test_cols():
+    result = []
+    result.append(cols())
+    test_columns = ['source_id', 'victim_name', 'victim_gender', 'victim_race',
+       'victim_age', 'victim_image_url', 'manner_of_injury', 'incident_date',
+       'address', 'city', 'state', 'zip', 'county', 'latitude', 'longitude',
+       'description', 'officer_outcomes', 'department', 'criminal_charges',
+       'source_link', 'off_duty_killing', 'encounter_type_draft',
+       'encounter_reason_draft', 'officer_names_draft', 'officer_races_draft',
+       'officer_known_past_shootings_draft', 'call_for_service_draft']
+    dataset = cols()
+    assert dataset == test_columns
+
+def test_isnan():
+    x = float("nan")
+    assert isnan(x) == True
+    assert isnan(1.5) == False
+
+def test_nan_to_none():
+    x = float("nan")
+    assert nan_to_none(x) == None
+
+def test_strip():
+    x = float("nan")
+    test_list = [1,2,3,4,x]
+    assert strip_nan(test_list) == [1,2,3,4]
+
+def test_map_df(df, mapper):
+    return [mapper(strip_nan(r)) for r in df.itertuples(index=False)]
+
+
+
+
+
+  
+
+
+        
+
+
