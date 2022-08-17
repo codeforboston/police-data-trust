627fa6a8e685b315940ba2b911576fb8d8a5f487
Logan-Dang <logandang100@yahoo.com>
2022-08-17 00:38:23 -0700

    remove commented code and clean

--- frontend/pages/officer/[id].tsx
+++ frontend/pages/officer/[id].tsx
@@ -7,45 +7,5 @@ import { useRouter } from "next/router"
 export default requireAuth(function OfficerPage() {
   const router = useRouter()
   const id = parseInt(router.query.id as string)
-  if (!isNaN(id)) {
-    const officer = getOfficerFromMockData(id)
-    return (
-      <Layout>
-        <OfficerView {...officer} />
-      </Layout>
-    )
-  } else {
-    return (
-      <Layout>
-        <p>Loading...</p>
-      </Layout>
-    )
-  }
-
-  // try {
-  //   const id = parseInt(router.query.id as string)
-  //   if (id == NaN) throw new Error()
-  //   console.log(id)
-  //   alert(id)
-  //   const officer = getOfficerFromMockData(id)
-  //   const tableProps = {
-  //     tableName: "Involved Incidents",
-  //     columns: resultsColumns,
-  //     data: EXISTING_TEST_INCIDENTS
-  //   }
-  //   return (
-  //     <Layout>
-  //       <OfficerHeader {...officer} />
-  //       <hr />
-  //       <OptionalOfficerInfo {...officer} />
-  //       <hr />
-  //       <OfficerWorkHistory {...officer} />
-  //       <OfficerAffiliations {...officer} />
-  //       <DataTable {...tableProps} />
-  //     </Layout>
-  //   )
-  // } catch (e) {
-  //   console.error(e)
-  //   return null
-  // }
+  return isNaN(id) ? <p>Loading</p> : <OfficerView {...getOfficerFromMockData(id)} />
 })
