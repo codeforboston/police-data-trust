160fdb69aa641f48022cf57f1be44f5e03887d50
ren0nie0 <32780767+ren0nie0@users.noreply.github.com>
2022-11-03 15:50:41 -0400

    removed print statement

--- backend/scraper/data_scrapers/counted/counted.py
+++ backend/scraper/data_scrapers/counted/counted.py
@@ -93,7 +93,6 @@ def col_conv():
             "lawenforcementagency": "department",
         },
     ).set_index("source_id", drop=False)
-    print(dataset)
     dataset = dataset[~dataset.index.duplicated(keep="first")]
     dataset = drop_existing_records(dataset, "counted")
     return dataset
