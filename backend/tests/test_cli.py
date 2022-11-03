b7c117fb03d662d737f3076d6d4ab4e2408f3740
ren0nie0 <32780767+ren0nie0@users.noreply.github.com>
2022-11-03 19:34:57 -0400

    removed test which has no corresponding function

--- backend/tests/test_cli.py
+++
@@ -1,6 +0,0 @@
-def test_shell_context_processor(app):
-    with app.app_context():
-        shell_globals = app.shell_context_processors[0]()
-        assert "app" in shell_globals
-        assert "client" in shell_globals
-        assert "db" in shell_globals
