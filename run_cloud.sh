74c59e11425529b1c1f49f5ee78cc83d6de412b5
Mike Yavorsky <mike.yavorsky@bioraft.com>
2023-07-18 21:14:23 -0400

    fix-port- | use env vars for ports on web, api containers

--- run_cloud.sh
+++ run_cloud.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export FLASK_ENV=${FLASK_ENV:-production}
+export PDT_API_PORT=${PDT_API_PORT:-5000}
 
 PYTHONPATH=. flask db seed
 # flask run --host=0.0.0.0
-gunicorn -w 2 --log-level=debug -b 0.0.0.0:5000 backend.wsgi:app
+gunicorn -w 2 --log-level=debug -b 0.0.0.0:$PDT_API_PORT backend.wsgi:app
