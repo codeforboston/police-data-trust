d8889d352c92c0e9be1569b208501fa37b9c9748
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-02-11 16:54:41 -0600

    make `PrimaryButton` receive forwarded "ref"

--- frontend/shared-components/primary-button/primary-button.tsx
+++ frontend/shared-components/primary-button/primary-button.tsx
@@ -4,17 +4,28 @@ import classnames from "classnames"
 
 export interface Props extends React.HTMLProps<HTMLButtonElement> {
   loading?: boolean
+  type?: "button" | "submit" | "reset"
 }
-export default function PrimaryButton({ className, loading, children, ...rest }: Props) {
+
+const Button = React.forwardRef<HTMLButtonElement, Props>(function PrimaryButton(
+  { className, loading, type, children, ...rest }: Props,
+  ref
+) {
   return (
-    <button {...rest} className={classnames(styles.primaryButton, className)} type="submit">
+    <button
+      ref={ref}
+      type={type || "submit"}
+      className={classnames(styles.primaryButton, className)}
+      {...rest}>
       {loading ? <Spinner /> : children}
     </button>
   )
-}
+})
 
 const Spinner = () => (
   <svg className={styles.spinner} viewBox="0 0 50 50">
     <circle className={styles.path} cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
   </svg>
 )
+
+export default Button
