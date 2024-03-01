57160229a768bb154647dad479b1f439973d01a0
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-03-01 16:29:34 -0600

    Add React Query/react-hot-toast setup in app providers

--- frontend/helpers/providers.tsx
+++ frontend/helpers/providers.tsx
@@ -1,12 +1,35 @@
-import { SearchProvider } from "./search"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Toaster } from "react-hot-toast"
 import { AuthProvider } from "./auth"
+import { SearchProvider } from "./search"
+
+/**
+ * Query Client responsible for housing cache of
+ * network requests, serves as basis
+ * for React Query functionality
+ */
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      retry: false
+    }
+  }
+})
 
 /**
  * Wraps components in application providers, which set up contexts to provide
  * services to components.
  */
 export const Providers: React.FC = ({ children }) => (
-  <AuthProvider>
-    <SearchProvider>{children}</SearchProvider>
-  </AuthProvider>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Toaster position="bottom-right" />
+      <AuthProvider>
+        <SearchProvider>{children}</SearchProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </>
 )
