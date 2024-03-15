5a12e2cb7cb0a47c26a59ccd6a22256473efbf73
Darrell Malone Jr <darrell@dkmalone.com>
2024-03-14 22:43:27 -0500

    Adds endpoint for creating and retreiving agencies - Create Agency - Get Agencies - Get Agency by ID
    Some incomplete/untested endpoints for updating
    and deleting agencies are also included.

--- backend/database/models/agency.py
+++ backend/database/models/agency.py
@@ -1,24 +1,24 @@
-import enum
-from .. import db
+from ..core import db, CrudMixin
+from enum import Enum
 
 
-class JURISDICTION(enum.Enum):
-    FEDERAL = 1
-    STATE = 2
-    COUNTY = 3
-    MUNICIPAL = 4
-    PRIVATE = 5
-    OTHER = 6
+class Jurisdiction(str, Enum):
+    FEDERAL = "FEDERAL"
+    STATE = "STATE"
+    COUNTY = "COUNTY"
+    MUNICIPAL = "MUNICIPAL"
+    PRIVATE = "PRIVATE"
+    OTHER = "OTHER"
 
 
-class Agency(db.Model):
+class Agency(db.Model, CrudMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     website_url = db.Column(db.Text)
     hq_address = db.Column(db.Text)
     hq_city = db.Column(db.Text)
     hq_zip = db.Column(db.Text)
-    jurisdiction = db.Column(db.Enum(JURISDICTION))
+    jurisdiction = db.Column(db.Enum(Jurisdiction))
 
     known_officers = db.relationship("Employment", back_populates="agency")
 
