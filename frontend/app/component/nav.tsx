b4b414436a261d6b4ec0adf20b1d8e978eb0148d
smckay1031 <smckay099@gmail.com>
2025-02-25 18:29:25 -0500

    installed next fonts

--- frontend/app/component/nav.tsx
+++ frontend/app/component/nav.tsx
@@ -1,22 +1,19 @@
 'use server'
+
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "@/public/images/NPDC_Logo_FINAL blue2 1.svg"
+
 export default async function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">Data explorer</Link>
-        </li>
-        <li>
-            <Link href="/community">Community</Link>
-        </li>
-        <li>
-            <Link href="/collection">Collection</Link>
-        </li>
-      </ul>
+      <div>
+        <Image src={Logo} alt="Logo" width={70} height={70} />
+      <div>
+        <p>National Police Data</p>
+        <p>The national index of police incidents </p>
+      </div>
+      </div>
     </nav>
   );
 }
