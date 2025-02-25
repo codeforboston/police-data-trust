0a2b683a1fb03b73f8bf4d6ae5d7dc0bb2be8444
smckay1031 <smckay099@gmail.com>
2025-02-24 21:52:54 -0500

    links added for top nav

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Nav from "@/app/component/nav";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Nav />
         {children}
       </body>
     </html>
