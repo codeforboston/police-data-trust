76ac2567f950d0767071d3f1eac9282819ca6df9
smckay1031 <smckay099@gmail.com>
2025-03-02 16:37:26 -0500

    sticky nav with icons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body className= {`${inter.variable} ${roboto}`}>
         <Nav />
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
