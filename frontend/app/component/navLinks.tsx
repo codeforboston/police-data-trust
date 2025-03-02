b4b4d1f4b4e040eb178e9b11cf31cf6cddcbe52f
smckay1031 <smckay099@gmail.com>
2025-03-02 14:23:55 -0500

    styled nav links with variable styling base in page url to display current page user is on

--- frontend/app/component/navLinks.tsx
+++ frontend/app/component/navLinks.tsx
@@ -1,23 +1,36 @@
 'use client'
 
-import Image from "next/image"
 import { usePathname } from "next/navigation"
+import Link from "next/link"
+import styles from "./navLinks.module.css";
 
 
 
 
-export default function NavLinks({props}) {
+interface NavLinkProps {
+    text: string;
+    href: string;
+}
+
+interface Props {
+    props: NavLinkProps[];
+}
+
+export default function NavLinks({ props }: Props) {
 
     const pathname = usePathname();
 
   return (
     <div>
-        <ul>
-            {props.map((prop) => (
+        <ul className={styles.links}>
+            {props.map((prop) => {
                 const isActive = pathname === prop.href;
-                return(
-                    <li key={`${props.text}`} className={ isAcitve? 'styles.activeLink' : 'styles.inactiveLink'} ></li>
+                return (
+                    <li key={`${prop.text}`} className={isActive ? `${styles.activeLink}` : `${styles.inactiveLink}`}>
+                        <Link href={prop.href}>{prop.text}</Link>
+                    </li>
                 )
+            })}
         </ul>
     </div>
 
