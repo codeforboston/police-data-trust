f0fb0fc6b7b02b48468dcde7675100903926aee0
smckay1031 <smckay099@gmail.com>
2025-03-16 18:55:20 -0400

    fully styled form text boxed but no functionality

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,8 +1,17 @@
+import React from "react";
 import styles from "./register.module.css";
+import  RegistrationForm  from "./registrationForm";
 import Image from "next/image";
 import logo from "@/public/images/NPDC_Logo_FINAL blue2 1.svg";
 import Box from "@mui/material/Container";
 import  TextField  from "@mui/material/TextField";
+import  InputAdornment  from "@mui/material/InputAdornment";
+import OutlinedInput  from "@mui/material/OutlinedInput";
+import  FormControl  from "@mui/material/FormControl";
+import Visibility  from "@mui/icons-material/Visibility";
+import  VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+
 
 
 
@@ -17,22 +26,3 @@ export default async function Register() {
 
 //Form for registration Client Component
 
-export function RegistrationForm(){
-
-    return (
-        <div>
-            <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '84vh', flexDirection: 'column'}}>
-                <Image src={logo} alt="NPDC Logo" width={100} height={100} />
-                <h1 className={styles.h1}>Create Account</h1>
-                <form className={styles.form} noValidate autoComplete="off">
-                    <TextField required id="email" label="Email" variant="outlined" sx={{width: '100%', py: "5px"}} />
-                    <TextField required id="first-name" label="First Name" variant="outlined" sx={{width: '100%', py: "10px"}}  />
-                    <TextField required id="last-name" label="Last Name" variant="outlined" sx={{width: '100%', py: "10px"}}  />
-                    <p className={styles.p}> Passwords must be at least 8 characters long while containing one upper case letter, lower case letter, and symbol.</p>
-                    <TextField required id="password" label="Password" variant="outlined" type="password" sx={{width: '100%', py: "10px"}}  />
-                    <TextField required id="confirm-password" label="Confirm Password" variant="outlined" type="password" sx={{width: '100%', py: "10px"}} />
-                </form>
-            </Box>
-        </div>
-    )
-}
