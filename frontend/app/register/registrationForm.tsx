89fa12932e8166bb906a2f6ec53b7a51db76d440
smckay1031 <smckay099@gmail.com>
2025-03-16 18:55:20 -0400

    fully styled form text boxed but no functionality

---
+++ frontend/app/register/registrationForm.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import React from "react";
+import styles from "./register.module.css";
+import Image from "next/image";
+import logo from "@/public/images/NPDC_Logo_FINAL blue2 1.svg";
+import Box from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import useState  from "react";
+import InputLabel from "@mui/material/InputLabel";
+import IconButton from "@mui/material/IconButton";
+
+export default function RegistrationForm() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "84vh",
+          flexDirection: "column",
+        }}
+      >
+        <Image src={logo} alt="NPDC Logo" width={100} height={100} />
+        <h1 className={styles.h1}>Create Account</h1>
+        <form className={styles.form} noValidate autoComplete="off">
+          <TextField
+            required
+            id="email"
+            label="Email"
+            variant="outlined"
+            sx={{ width: "100%", py: "5px" }}
+            margin="dense"
+          />
+          <TextField
+            required
+            id="first-name"
+            label="First Name"
+            variant="outlined"
+            sx={{ width: "100%" }}
+            margin="dense"
+          />
+          <TextField
+            required
+            id="last-name"
+            label="Last Name"
+            variant="outlined"
+            sx={{ width: "100%" }}
+            margin="dense"
+          />
+          <p className={styles.p}>
+            {" "}
+            Passwords must be at least 8 characters long while containing one
+            upper case letter, lower case letter, and symbol.
+          </p>
+          <FormControl sx={{ marginY: '5px', width: "100%" }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword
+                        ? "hide the password"
+                        : "display the password"
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <FormControl
+            varient="outlined"
+            sx={{ width: "100%", marginY: "5px" }}
+          >
+            <InputLabel>Confirm Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword
+                        ? "hide the password"
+                        : "display the password"
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Confirm Password"
+            />
+          </FormControl>
+        </form>
+      </Box>
+    </div>
+  );
+}
