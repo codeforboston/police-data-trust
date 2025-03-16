778cec3ac23435d9bc3b3d97d226731859a43aee
smckay1031 <smckay099@gmail.com>
2025-03-16 15:25:40 -0400

    mobile nav bug fixed

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./register.module.css";
+
+export default async function Register() {
+    return (
+        <div className={styles.main}>
+            <h1> Register New User </h1>
+        </div>
+    );
+}
