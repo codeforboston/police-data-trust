761bcc8712d5a2ad334a3f80542c12ceca1362c7
Sean Nealon <signalflowsean@macbookpro.mynetworksettings.com>
2025-05-29 09:52:45 -0400

    delete old frontend

--- frontend/compositions/visualizations/map/__mocks__/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function MockMap() {
-  return <div>FakeMap</div>
-}
--- frontend/models/mock-data/index.ts
+++
@@ -1,7 +0,0 @@
-import mockIncident from "./incidents.json"
-import mockIncidentG from "./grammy.json"
-import mockSavedResults from "./saved-results.json"
-import mockSavedSearches from "./saved-searches.json"
-import mockUsers from "./users.json"
-
-export { mockIncident, mockIncidentG, mockSavedResults, mockSavedSearches, mockUsers }
