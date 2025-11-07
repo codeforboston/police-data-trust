ed59316cd4ab579f2a58613f41b1e243e4effaf5
Darrell Malone Jr. <darrell@dkmalone.com>
2025-11-06 20:02:29 -0600

    Revert "Fixes some common development issues. (#494)" (#496)
    This reverts commit b4b5d4314b34f57ed9281918d7281fbacca56909.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,4 @@ const nextConfig: NextConfig = {
   output: "export"
 }
 
-module.exports = {
-  allowedDevOrigins: ["http://localhost:3000", "http://127.0.0.1:3000", "127.0.0.1"]
-}
-
 export default nextConfig
