481b15db2c95b44e3bef3a069a5bf437b6614fe7
Joseph Richardson <richardson.joseph1990@gmail.com>
2026-01-29 18:37:22 -0800

    adding at query level to get all queries to match the fuzzy search query

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -309,8 +309,9 @@ def text_search():
     q_page = args.get("page", 1, type=int)
     q_per_page = args.get("per_page", 20, type=int)
     query = args.get("query", None, type=str)
+    updated_query_fuzzy = query + "*"
     params = {
-        "query": query,
+        "query": updated_query_fuzzy,
         "page": q_page,
         "per_page": q_per_page
     }
