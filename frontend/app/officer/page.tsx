6f51f9f3fd0baf3542851aa7412deb618e3592e9
Darrell Malone Jr. <darrell@dkmalone.com>
2026-03-03 22:06:24 -0600

    Revert "Adds links to search results. (#537)" (#538)
    This reverts commit a89c6d1afd5ac58a399b5582d84eb9eaad41a504.

--- frontend/app/officer/page.tsx
+++ frontend/app/officer/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { useAuth } from "@/providers/AuthProvider"
 import { apiFetch } from "@/utils/apiFetch"
 import API_ROUTES, { apiBaseUrl } from "@/utils/apiRoutes"
-import { useParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import { Officer } from "@/utils/api"
 import DetailsLayout from "@/components/Details/DetailsLayout"
 import IdentityCard from "@/components/Details/IdentityCard"
@@ -12,29 +12,26 @@ import DetailsTabs from "@/components/Details/DetailsTabs"
 import ContentDetails from "@/components/Details/ContentDetails"
 
 export default function OfficerDetailsPage() {
-  const params = useParams<{ uid: string }>()
-  const uid = params.uid
-
+  const searchParams = useSearchParams()
+  const uid = searchParams.get("uid")
   const [officer, setOfficer] = useState<Officer | null>(null)
   const { accessToken } = useAuth()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    if (!accessToken || !uid) return
-
-    setLoading(true)
-
-    apiFetch(
-      `${apiBaseUrl}${API_ROUTES.officers.profile(uid)}?include=employment&include=allegations`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
+    if (accessToken && uid) {
+      apiFetch(
+        `${apiBaseUrl}${API_ROUTES.officers.profile(uid)}?include=employment&include=allegations`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
         }
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => setOfficer(data.results || data))
-      .finally(() => setLoading(false))
+      )
+        .then((res) => res.json())
+        .then((data) => setOfficer(data.results || data))
+        .finally(() => setLoading(false))
+    }
   }, [accessToken, uid])
 
   if (loading) return <div>Loading...</div>
