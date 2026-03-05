fa3c8a076ac38b8928fa14aa25a4c0c0e5ed53a7
Darrell K Malone Jr. <darrell@dkmalone.com>
2026-03-04 22:33:34 -0600

    Fix formatting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: "standalone"
 }
 
 export default nextConfig
