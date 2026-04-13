d8c90087aeeb49db31c114d0036926aa36444ec3
Darrell K Malone Jr. <darrell@dkmalone.com>
2026-04-13 07:30:33 -0400

    Update the front end vis for launch

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,14 @@
 "use client"
 import styles from "./page.module.css"
 import SearchBar from "@/components/SearchBar"
-import LatestUpdates from "@/components/LatestUpdatesSection"
-import UpToDateNotification from "@/components/UpToDateNotification"
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.heading}>How can we help you?</h1>
-      <SearchBar />
-      <LatestUpdates />
-      <UpToDateNotification />
+      <section className={styles.hero}>
+        <h1 className={styles.heading}>How can we help you?</h1>
+        <SearchBar />
+      </section>
     </div>
   )
 }
